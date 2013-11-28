4a71dc0a0beb27549d78b032b48d2daacd875a22
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-11-28 15:44:34 +0100

    Added signal to add user to workspace participants on create of 83b

--- toolkit/apps/workspace/models.py
+++ toolkit/apps/workspace/models.py
@@ -94,4 +94,5 @@ Import workspace signals
 """
 from .signals import (ensure_workspace_slug,
                      ensure_workspace_has_83b_by_default,
+                     ensure_83b_user_in_workspace_participants,
                      ensure_tool_slug)
