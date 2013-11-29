7563e1e550b9ee0a1e80a64efe32996692d1b198
Jamie Connolly <jamie@jamieconnolly.com>
2013-11-29 15:57:49 +0000

    Add in some extra information to the 83b model

--- toolkit/apps/eightythreeb/models.py
+++ toolkit/apps/eightythreeb/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+from jsonfield import JSONField
+
 from django.db import models
 from django.core.urlresolvers import reverse
-
-from jsonfield import JSONField
 
 
 class EightyThreeB(models.Model):
@@ -14,7 +15,19 @@ class EightyThreeB(models.Model):
     data = JSONField(default={})
 
     def __unicode__(self):
-        return u'83b for %s' % self.user.get_full_name()
+        return u'83(b) for %s' % self.client_name
+
+    @property
+    def client_name(self):
+        return self.data.get('client_full_name', None)
+
+    @property
+    def filing_date(self):
+        return self.transfer_date + timedelta(days=30)
+
+    @property
+    def transfer_date(self):
+        return datetime.strptime(self.data.get('date_of_property_transfer', None), '%Y-%m-%d').date()
 
     def get_absolute_url(self):
         return reverse('eightythreeb:view')
