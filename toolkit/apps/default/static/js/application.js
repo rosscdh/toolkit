d0a672f02cfa05274b7e85b0223ca96e8cc917f2
Jamie Connolly <jamie@jamieconnolly.com>
2013-11-29 17:30:56 +0000

    Removed a lot of redundant code

--- toolkit/apps/default/static/js/application.js
+++ toolkit/apps/default/static/js/application.js
@@ -6,15 +6,6 @@ String.prototype.repeat = function(num) {
 
 (function($) {
 
-  // Add segments to a slider
-  $.fn.addSliderSegments = function (amount) {
-    return this.each(function () {
-      var segmentGap = 100 / (amount - 1) + "%"
-        , segment = "<div class='ui-slider-segment' style='margin-left: " + segmentGap + ";'></div>";
-      $(this).prepend(segment.repeat(amount - 2));
-    });
-  };
-
   $(function() {
 
     // Custom Selects
@@ -35,58 +26,6 @@ String.prototype.repeat = function(num) {
     // Tags Input
     $(".tagsinput").tagsInput();
 
-    // jQuery UI Sliders
-    var $slider = $("#slider");
-    if ($slider.length > 0) {
-      $slider.slider({
-        min: 1,
-        max: 5,
-        value: 3,
-        orientation: "horizontal",
-        range: "min"
-      }).addSliderSegments($slider.slider("option").max);
-    }
-
-    var $slider2 = $("#slider2");
-    if ($slider2.length > 0) {
-      $slider2.slider({
-        min: 1,
-        max: 5,
-        values: [3, 4],
-        orientation: "horizontal",
-        range: true
-      }).addSliderSegments($slider2.slider("option").max);
-    }
-
-    var $slider3 = $("#slider3")
-      , slider3ValueMultiplier = 100
-      , slider3Options;
-
-    if ($slider3.length > 0) {
-      $slider3.slider({
-        min: 1,
-        max: 5,
-        values: [3, 4],
-        orientation: "horizontal",
-        range: true,
-        slide: function(event, ui) {
-          $slider3.find(".ui-slider-value:first")
-            .text("$" + ui.values[0] * slider3ValueMultiplier)
-            .end()
-            .find(".ui-slider-value:last")
-            .text("$" + ui.values[1] * slider3ValueMultiplier);
-        }
-      });
-
-      slider3Options = $slider3.slider("option");
-      $slider3.addSliderSegments(slider3Options.max)
-        .find(".ui-slider-value:first")
-        .text("$" + slider3Options.values[0] * slider3ValueMultiplier)
-        .end()
-        .find(".ui-slider-value:last")
-        .text("$" + slider3Options.values[1] * slider3ValueMultiplier);
-    }
-
     // Add style class name to a tooltips
     $(".tooltip").addClass(function() {
       if ($(this).prev().attr("data-tooltip-style")) {
@@ -105,35 +44,6 @@ String.prototype.repeat = function(num) {
     $(".btn-group a").on('click', function() {
       $(this).siblings().removeClass("active").end().addClass("active");
     });
-
-    // Disable link clicks to prevent page scrolling
-    $('a[href="#fakelink"]').on('click', function (e) {
-      e.preventDefault();
-    });
-
-    // jQuery UI Spinner
-    $.widget( "ui.customspinner", $.ui.spinner, {
-      widgetEventPrefix: $.ui.spinner.prototype.widgetEventPrefix,
-      _buttonHtml: function() { // Remove arrows on the buttons
-        return "" +
-        "<a class='ui-spinner-button ui-spinner-up ui-corner-tr'>" +
-          "<span class='ui-icon " + this.options.icons.up + "'></span>" +
-        "</a>" +
-        "<a class='ui-spinner-button ui-spinner-down ui-corner-br'>" +
-          "<span class='ui-icon " + this.options.icons.down + "'></span>" +
-        "</a>";
-      }
-    });
-
-    $('#spinner-01').customspinner({
-      min: -99,
-      max: 99
-    }).on('focus', function () {
-      $(this).closest('.ui-spinner').addClass('focus');
-    }).on('blur', function () {
-      $(this).closest('.ui-spinner').removeClass('focus');
-    });
-
 
     // Focus state for append/prepend inputs
     $('.input-group').on('focus', '.form-control', function () {
@@ -161,7 +71,7 @@ String.prototype.repeat = function(num) {
     });
 
     // jQuery UI Datepicker
-    var datepickerSelector = '#datepicker-01';
+    var datepickerSelector = '.datepicker';
     $(datepickerSelector).datepicker({
       showOtherMonths: true,
       selectOtherMonths: true,
