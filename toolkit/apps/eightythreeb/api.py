1c376f9ff1b3d83db8c53a2f91c08318bef7b939
Jamie Connolly <jamie@jamieconnolly.com>
2013-12-04 10:57:25 +0000

    Fix a load of Pep8 issues

--- toolkit/apps/eightythreeb/api.py
+++ toolkit/apps/eightythreeb/api.py
@@ -1,7 +1,4 @@
 # -*- coding: UTF-8 -*-
-from django.contrib.auth.models import User
-from django.core.exceptions import PermissionDenied
-
 from rest_framework.viewsets import ModelViewSet
 
 from .models import EightyThreeB
