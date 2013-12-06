77fe83ef9e0fd40d2d8d2f1f0c6d3749ad2eb5c0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-06 16:41:36 +0100

    Added model manager and got process step through working

--- toolkit/apps/eightythreeb/__init__.py
+++ toolkit/apps/eightythreeb/__init__.py
@@ -9,7 +9,7 @@ EIGHTYTHREEB_STATUS = get_namedtuple_choices('EIGHTYTHREEB_STATUS', (
     (4, 'customer_print_and_sign', 'Customer to Print and sign 83b Election Form'),  # customer - changed to when the customer completes the form
     (5, 'mail_to_irs_tracking_code', 'Customer to Mail to IRS & register Tracking Code'),  # customer; collect usps registration code here - changed to after user indicates they have printed and signed the form
     (6, 'irs_recieved', 'Waiting for reciept of 83b by IRS (via USPS)'),  # webhook callback/cron to query usps for this status
-    (7, 'datestampped_copy_recieved', 'Date stamped copy received by Customer'),  # attorney/customer - attorney/customer indicates they recieved the copy (may need to upload scan?)
+    (7, 'datestamped_copy_recieved', 'Date stamped copy received by Customer'),  # attorney/customer - attorney/customer indicates they recieved the copy (may need to upload scan?)
     (8, 'copy_sent_to_lawyer', 'Customer to send copy of date-stamped doc to Lawyer'),  # attorney/customer sends copy to company (? depending on 5.)
     (9, 'copy_sent_to_accountant', 'Customer to send copy of date-stamped doc to accountant'),  # attorney/customer sends copy to accountant (?)
     (10, 'complete', 'Process Complete'),  # attorney/customer sends copy to accountant (?)
