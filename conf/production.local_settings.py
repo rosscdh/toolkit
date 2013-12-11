4731d86520992abf0cc86b94af358fe713e83a2e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-11 13:58:41 +0100

    added prod settings

---
+++ conf/production.local_settings.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+LOCAL_SETTINGS = True
+from settings import *
+
+PROJECT_ENVIRONMENT = 'prod'
+
+SITE_ID = 1
+
+SECRET_KEY = 'i6=)1=4in#zyp&amp;g)^9nqodjgjru134)@2)^$ox5w7ac*uhml!uy-5'
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'toolkit_production',
+        'USER': 'postgres',
+        'PASSWORD': 'p7vgff9h197gnres0kj13btoos4',
+        'HOST': 'ec2-50-18-97-221.us-west-1.compute.amazonaws.com',
+        'PORT': 5432
+    }
+}
+
+USE_ETAGS = True
+
+MEDIA_URL = '/m/'
+STATIC_URL = '/s/'
+
+STATIC_ROOT = '/var/apps/toolkit/static/'
+MEDIA_ROOT = '/var/apps/toolkit/media/'
+
+ALLOWED_HOSTS = ['*']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'lawpal'
+EMAIL_HOST_PASSWORD = '0113633alex'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'noreply@lawpal.com'
+SERVER_EMAIL = 'glynt@lawpal.com'
+
+
+RAVEN_CONFIG = {
+    'dsn': 'https://b5a6429d03e2418cbe71cd5a4c9faca6:ddabb51af47546d1ac0e63cb453797ba@app.getsentry.com/6287',
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211'
+    },
+    'session_cache': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'TIMEOUT': 3600
+    },
+    'fallback': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/tmp/glynt.production.cache',
+    }
+}
