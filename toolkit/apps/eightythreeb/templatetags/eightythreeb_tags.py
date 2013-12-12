336db22975392176853c4df8bd8317cf6ee5c4d9
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-12 11:35:50 +0100

    getting its mixin sorted and injected object into context as well as cleaned html as well as included the irs_page x 2

--- toolkit/apps/eightythreeb/templatetags/eightythreeb_tags.py
+++ toolkit/apps/eightythreeb/templatetags/eightythreeb_tags.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 from django import template
 
+from dateutil import parser
+
 register = template.Library()
 
+
+@register.filter(name='to_date')
+def to_date(value):
+    if type(value) in [str, unicode]:
+        return parser.parse(value)
+    return value
