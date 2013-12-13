a95cc5304c2c83739f88decd9e214a91639e2ecb
Jamie Connolly <jamie@jamieconnolly.com>
2013-12-13 16:42:05 +0000

    Fixed up the Workspace.objects.mine check, and removed the DB call that isn't needed

--- toolkit/apps/workspace/managers.py
+++ toolkit/apps/workspace/managers.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class WorkspaceManager(models.Manager):
     def mine(self, user):
-        return super(WorkspaceManager, self).get_query_set().filter(participants=user)
+        if not user.is_authenticated():
+            return []
+
+        return super(WorkspaceManager, self).get_query_set().filter(participants__in=[user])
