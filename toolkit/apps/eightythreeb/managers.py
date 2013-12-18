d96d611171b8714cf538c0a3493ae064b6389096
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-18 14:46:56 +0100

    added incomplete 83b objects handler

--- toolkit/apps/eightythreeb/managers.py
+++ toolkit/apps/eightythreeb/managers.py
@@ -1,8 +1,18 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
 
+import datetime
+
 
 class EightyThreeBManager(models.Manager):
+    """
+    Default objects manager with helper methods
+    """
     def awaiting_tracking_code(self):
         # exclude users who have not set their password
         return super(EightyThreeBManager, self).get_query_set().filter(status=self.model.STATUS_83b.irs_recieved)
+
+    def incomplete(self):
+        return super(EightyThreeBManager, self).get_query_set() \
+                                                .exclude(status=self.model.STATUS_83b.complete) \
+                                                .filter(filing_date__gte=datetime.date.today())
