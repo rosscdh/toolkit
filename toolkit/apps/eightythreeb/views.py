af0c7134627afc6162443719392ffc590f8f3b5b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-20 13:00:59 +0100

    Added view and urls and templates for shooing the form

--- toolkit/apps/eightythreeb/views.py
+++ toolkit/apps/eightythreeb/views.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponseRedirect
 from django.views.generic import FormView, UpdateView
 
 from toolkit.apps.workspace.mixins import IssueSignalsMixin
 
 from .models import EightyThreeB
-from .forms import TrackingCodeForm
+from .forms import TrackingCodeForm, AttachmentForm
 
 
 class TrackingCodeView(IssueSignalsMixin, UpdateView):
@@ -19,3 +18,17 @@ class TrackingCodeView(IssueSignalsMixin, UpdateView):
         self.object = form.save()
         self.issue_signals(request=self.request, instance=self.object, name='mail_to_irs_tracking_code')
         return super(TrackingCodeView, self).form_valid(form)
+
+
+class AttachmentView(IssueSignalsMixin, UpdateView):
+    model = EightyThreeB
+    form_class = AttachmentForm
+    template_name_suffix = '_attachment_form'
+
+    def form_valid(self, form):
+        """
+        Issue the object signals on save
+        """
+        self.object = form.save()
+        self.issue_signals(request=self.request, instance=self.object, name='copy_uploaded')
+        return super(AttachmentView, self).form_valid(form)
