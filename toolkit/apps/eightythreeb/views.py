ae38f1f9473c713a4c5fa9218027f1954bab64dc
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-23 13:40:16 +0100

    Got basic uploader view working need to make it a service

--- toolkit/apps/eightythreeb/views.py
+++ toolkit/apps/eightythreeb/views.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import FormView, UpdateView
+from django.conf import settings
+from django.core.files import File
+from django.views.generic import UpdateView, DetailView
+
+from ajaxuploader.views import AjaxFileUploader
+from ajaxuploader.backends.default_storage import DefaultStorageUploadBackend
 
 from toolkit.apps.workspace.mixins import IssueSignalsMixin
 
 from .models import EightyThreeB
 from .forms import TrackingCodeForm, AttachmentForm
+
+import os
+import json
+import logging
+logger = logging.getLogger('django.request')
 
 
 class TrackingCodeView(IssueSignalsMixin, UpdateView):
@@ -32,3 +42,38 @@ class AttachmentView(IssueSignalsMixin, UpdateView):
         self.object = form.save()
         self.issue_signals(request=self.request, instance=self.object, name='copy_uploaded')
         return super(AttachmentView, self).form_valid(form)
+
+
+class UploadFileView(UpdateView):
+    """
+    Override the uploader and cater to the multiple file associations
+    @TODO turn this into a service
+    """
+    model = EightyThreeB
+    uploader = None
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileView, self).__init__(*args, **kwargs)
+        self.uploader = AjaxFileUploader()
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        # Upload the File using the ajax_uploader object
+        response = self.uploader._ajax_upload(request, *args, **kwargs)
+        if response.status_code in [200]:
+            # success
+            data = json.loads(response.content)
+            base_path, filename = os.path.split(data.get('path'))
+            # create and upload to s3
+            with open(os.path.join(settings.MEDIA_ROOT, 'uploads', filename)) as file_path:
+                uploaded_file = File(file_path)
+                attachment = self.object.attachment_set.create(eightythreeb=self.object)
+                attachment.attachment = uploaded_file
+                attachment.save()
+
+            os.remove(uploaded_file.name)
+
+            data['path'] = attachment.attachment.url
+            response.content = json.dumps(data)
+
+        return response
