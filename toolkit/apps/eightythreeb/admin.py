3c29106b54fc17bf6251387cbcd3e528e1f46576
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-23 13:39:56 +0100

    changed for signal attachment to multiple

--- toolkit/apps/eightythreeb/admin.py
+++ toolkit/apps/eightythreeb/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import EightyThreeB
+from .models import EightyThreeB, Attachment
 
 
-admin.site.register([EightyThreeB])
+admin.site.register([EightyThreeB, Attachment])
