86d56945a92a8de891f05a497c55e0ae1185a09f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-27 15:02:03 +0100

    made function helper available in module

--- toolkit/casper/__init__.py
+++ toolkit/casper/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .base import BaseCasperJs
 
-from .prettify import httprettify_methods
+from .prettify import httprettify_methods, mock_http_requests
 
 from .workflow_case import (PyQueryMixin,
                             BaseProjectCaseMixin)
