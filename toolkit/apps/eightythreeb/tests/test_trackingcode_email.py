4171790f7f07dc75c3b46040920e4b228033fb3f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2013-12-30 16:30:40 +0100

    Added attachments handlers… may cause slowness in sending of event

--- toolkit/apps/eightythreeb/tests/test_trackingcode_email.py
+++ toolkit/apps/eightythreeb/tests/test_trackingcode_email.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.core import mail
 from django.test import TestCase
+from django.core.files import File
 from django.db.models.query import QuerySet
+from django.core.files.storage import FileSystemStorage
 
+import os
+import mock
 import datetime
 import httpretty
 
@@ -12,12 +16,14 @@ from usps.api.tracking import TrackConfirmWithFields
 from toolkit.casper import httprettify_methods, mock_http_requests
 from toolkit.apps.workspace.services import USPSTrackingService, USPSResponse
 
-from .data import EIGHTYTHREEB_TRACKINGCODE_DATA
-from .usps_trackfield_response import TRACK_UNDELIVERED_RESPONSE_XML_BODY
-
 from toolkit.apps.workspace.models import Tool
 from toolkit.apps.eightythreeb.models import EightyThreeB
 from toolkit.apps.eightythreeb.management.commands.eightythreeb_usps_track_response import Command as USPSEightyThreeBTracking
+
+from .data import EIGHTYTHREEB_TRACKINGCODE_DATA
+from .usps_trackfield_response import TRACK_UNDELIVERED_RESPONSE_XML_BODY
+
+FILE_BASE_PATH = os.path.dirname(__file__)
 
 TRACKING_CODE = 'EJ958083578US'
 
@@ -28,7 +34,10 @@ class BaseUSPSTrackingCode(TestCase):
     """
     fixtures = ['sites', 'tools']
 
+    @mock.patch('storages.backends.s3boto.S3BotoStorage', FileSystemStorage)
     def setUp(self):
+        super(BaseUSPSTrackingCode, self).setUp()
+
         self.subject = USPSEightyThreeBTracking()
 
         self.user = mommy.make('auth.User', first_name='Customer', last_name='Test', email='test+customer@lawpal.com')
@@ -48,8 +57,13 @@ class BaseUSPSTrackingCode(TestCase):
                                        transfer_date=datetime.date.today(),
                                        status=EightyThreeB.STATUS_83b.mail_to_irs_tracking_code)
 
+        self.eightythreeb.attachment_set.create(attachment=File(open('%s/attachment-1.gif' % FILE_BASE_PATH, 'r')))
+        self.eightythreeb.attachment_set.create(attachment=File(open('%s/attachment-2.jpg' % FILE_BASE_PATH, 'r')))
+        self.eightythreeb.attachment_set.create(attachment=File(open('%s/attachment-3.pdf' % FILE_BASE_PATH, 'r')))
+
 
 class TestTrackingCodeEntered(BaseUSPSTrackingCode):
+
     def setUp(self):
         super(TestTrackingCodeEntered, self).setUp()
         self.eightythreeb.tracking_code = TRACKING_CODE
@@ -64,21 +78,25 @@ class TestTrackingCodeEntered(BaseUSPSTrackingCode):
         self.eightythreeb.base_signal.send(sender=self, instance=self.eightythreeb, actor=self.eightythreeb.user, name='mail_to_irs_tracking_code')
         # reload the model data
         self.eightythreeb = self.eightythreeb._meta.model.objects.get(pk=self.eightythreeb.pk)
+
         # tests
         self.assertTrue('mail_to_irs_tracking_code' in self.eightythreeb.data['markers'])
 
         self.assertEqual(len(mail.outbox), 2)  # to customer AND lawyer @TODO and accountant too???
 
+        num_attachments = self.eightythreeb.attachment_set.all().count()
+
         email = mail.outbox[0]
+
         self.assertEqual(email.subject, u'83b Tracking Code entered for Customer Test')
+        self.assertEqual(len(email.attachments), num_attachments)  # test we have the attachments
         self.assertEqual(len(email.to), 1)
         self.assertEqual(email.to, ['test+customer@lawpal.com'])
         self.assertEqual(email.from_email, 'tech@lawpal.com')
 
         email = mail.outbox[1]
         self.assertEqual(email.subject, u'83b Tracking Code entered for Customer Test')
+        self.assertEqual(len(email.attachments), num_attachments)  # test we have the attachments
         self.assertEqual(len(email.to), 1)
         self.assertEqual(email.to, ['test+lawyer@lawpal.com'])
         self.assertEqual(email.from_email, 'tech@lawpal.com')
-
-        #mail_to_irs_tracking_code
