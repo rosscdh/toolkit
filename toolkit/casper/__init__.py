b841241d983a80f97da87a43bc5b6ec9803ff70d
Ross Crawford <sendrossemail@gmail.com>
2013-12-31 14:43:42 +0100

    getting user redirect tests working

--- toolkit/casper/__init__.py
+++ toolkit/casper/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from .base import BaseCasperJs
 
+from .workflow_case import (PyQueryMixin,
+                            BaseScenarios,
+                            BaseProjectCaseMixin)
+
 from .prettify import httprettify_methods, mock_http_requests
 
-from .workflow_case import (PyQueryMixin,
-                            BaseProjectCaseMixin)
