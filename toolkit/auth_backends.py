8ca8bd2b7750f6108c6327f081f56fca9747c802
Ross Crawford <sendrossemail@gmail.com>
2013-12-31 13:15:27 +0100

    made logger lowercase cos its ugly as uppercase

--- toolkit/auth_backends.py
+++ toolkit/auth_backends.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User, check_password
 from toolkit.apps.workspace.models import InviteKey
 
 import logging
-LOGGER = logging.getLogger('django.request')
+logger = logging.getLogger('django.request')
 
 
 class EmailBackend(object):
@@ -18,7 +18,7 @@ class EmailBackend(object):
             user = User.objects.get(email=username)
             pwd_valid = check_password(password, user.password)
         except User.DoesNotExist:
-            LOGGER.error('User does not exist: %s' % username)
+            logger.error('User does not exist: %s' % username)
 
         if user and pwd_valid:
             return user
@@ -36,17 +36,16 @@ class SecretKeyBackend(EmailBackend):
     def authenticate(self, username=None, password=None):
         # Check the username/password and return a User.
         user = None
-        pwd_valid = False
 
         try:
             invite = InviteKey.objects.get(key=username)
             if invite.has_been_used is True:
-                LOGGER.error('InviteKey has already been used: %s' % username)
+                logger.error('InviteKey has already been used: %s' % username)
                 raise ObjectDoesNotExist
 
             user = invite.invited_user
 
         except Exception as e:
-            LOGGER.error('InviteKey does not exist: %s reason: %s' % (username, e))
+            logger.error('InviteKey does not exist: %s reason: %s' % (username, e))
 
         return user
