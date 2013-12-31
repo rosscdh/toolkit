46f92b2543a7368f8b86af4ae742939661fd50c4
Ross Crawford <sendrossemail@gmail.com>
2013-12-31 15:40:04 +0100

    Added basic tests for user login and invited

--- toolkit/apps/me/tests/test_customer.py
+++ toolkit/apps/me/tests/test_customer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 from model_mommy import mommy
 
@@ -11,6 +12,11 @@ class BaseCustomer(BaseProjectCaseMixin):
     def setUp(self):
         super(BaseCustomer, self).setUp()
         self.basic_workspace()
+
+        # reset userpass
+        self.user.password = ''
+        self.user.save(update_fields=['password'])
+
         self.invite = mommy.make('workspace.InviteKey', invited_user=self.user,
                                  inviting_user=self.lawyer,
                                  tool=Tool.objects.get(slug='83b-election-letters'),
@@ -26,12 +32,22 @@ class CustomerInviteLoginTest(BaseCustomer):
     """
     def test_invited_login(self):
         """
-        test invite url logs user in
+        Customer invited and has no pasword set
+        is redirected to confirm account page
         """
         resp = self.client.get(self.invite.get_absolute_url(), follow=True)
-        self.assertEqual(resp.status_code, 200)
-        import pdb;pdb.set_trace()
+        self.assertEqual(len(resp.redirect_chain), 2)
+        self.assertEqual(resp.redirect_chain[1], ('http://testserver%s' % reverse('me:confirm-account'), 302))
 
+    def test_password_set_login(self):
+        """
+        Customer logs in after having set password
+        is redirected to last invited object
+        """
+        self.user.set_password('test')
+        self.user.save(update_fields=['password'])
 
-class CustomerInviteLoginRedirectTest(TestCase):
-    pass
+        resp = self.client.get(self.invite.get_absolute_url(), follow=True)
+        self.assertEqual(len(resp.redirect_chain), 1)
+        # redirects to the eighty threeb (the last invite sent to this user object)
+        self.assertEqual(resp.redirect_chain[0], ('http://testserver%s' % self.eightythreeb.get_absolute_url(), 302))
