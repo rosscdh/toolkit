4106c7559e766ddc90f0b29257a8801b244e4610
Ross Crawford <sendrossemail@gmail.com>
2013-12-31 13:15:11 +0100

    added vies and forms to handle confirm-account form and redirects to that based on the invite key and all future login attempts

--- toolkit/apps/me/views.py
+++ toolkit/apps/me/views.py
@@ -2,10 +2,25 @@ from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView, UpdateView
 
-from .forms import ChangePasswordForm, AccountSettingsForm
+from toolkit.apps.workspace.models import InviteKey
+
+from .forms import ConfirmAccountForm, ChangePasswordForm, AccountSettingsForm
 
 
 User = get_user_model()
+
+
+class ConfirmAccountView(UpdateView):
+    form_class = ConfirmAccountForm
+    model = User
+    template_name = 'user/settings/account.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_success_url(self):
+        first_invite_key = InviteKey.objects.filter(invited_user=self.request.user).first()
+        return first_invite_key.next
 
 
 class AccountSettingsView(UpdateView):
