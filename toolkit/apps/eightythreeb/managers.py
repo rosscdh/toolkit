d40e11f1d00c1935d5d7596ce26c633ec44a3d4b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-02 12:39:20 +0100

    Created isdeletedmixin and query set object which ensure that if a model has an is_deleted field they are not deleted but instead are set to is_deleted = True this happens on the queryet level too

--- toolkit/apps/eightythreeb/managers.py
+++ toolkit/apps/eightythreeb/managers.py
@@ -1,7 +1,19 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
+from django.db.models.query import QuerySet
 
 import datetime
+
+
+class isDeletedQuerySet(QuerySet):
+    """
+    Mixin to override the Queryset.delete() functionality of a manager
+    """
+    def delete(self, *args, **kwargs):
+        if 'is_deleted' in self.model._meta.get_all_field_names():
+            self.update(is_deleted=True)
+        else:
+            super(isDeletedQuerySet, self).delete()
 
 
 class EightyThreeBManager(models.Manager):
@@ -22,7 +34,7 @@ class AttachmentManger(models.Manager):
     Manager for 83b attachments
     """
     def get_query_set(self):
-        return super(AttachmentManger, self).get_query_set().filter(is_deleted=False)
+        return isDeletedQuerySet(self.model, using=self._db).filter(is_deleted=False)
 
     def deleted(self):
         return super(AttachmentManger, self).get_query_set().filter(is_deleted=True)
--- toolkit/apps/eightythreeb/mixins.py
+++ toolkit/apps/eightythreeb/mixins.py
@@ -7,6 +7,18 @@ from datetime import date, datetime, timedelta
 from toolkit.apps.workspace.services import USPSResponse
 
 
+class IsDeletedMixin(object):
+    def delete(self, *args, **kwargs):
+        """
+        override delete and set is_deleted = True if we have that attrib
+        """
+        if hasattr(self, 'is_deleted'):
+            self.is_deleted = True
+            self.save(update_fields=['is_deleted'])
+        else:
+            super(IsDeletedMixin, self).delete(*args, **kwargs)
+
+
 class HTMLMixin(object):
     @property
     def template(self):
