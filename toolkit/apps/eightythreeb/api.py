6c1740e1e2055a90f171c87a62aee8b666bd7140
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-02 12:38:05 +0100

    removed custom is_delete override in api endpoint because we now handle it at the ORM level

--- toolkit/apps/eightythreeb/api.py
+++ toolkit/apps/eightythreeb/api.py
@@ -37,14 +37,3 @@ class AttachmentDeleteView(DestroyAPIView, UpdateModelMixin):
     """
     queryset = Attachment.objects.all()
     serializer_class = AttachmentSerializer
-
-    def destroy(self, request, *args, **kwargs):
-        """
-        Override the destroy method as we want to updated is_deleted
-        not actaully delete the object
-        """
-        # manually set the is_deleted True
-        request.DATA['is_deleted'] = True
-
-        # partial_update because were PATCHING the object
-        return self.partial_update(request, *args, **kwargs)
