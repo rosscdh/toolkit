e36f04b38a17bbd3ef3f8fbfededebc438fd82c9
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-03 08:49:46 +0100

    Fix for broken date

--- toolkit/apps/eightythreeb/templatetags/eightythreeb_tags.py
+++ toolkit/apps/eightythreeb/templatetags/eightythreeb_tags.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from django import template
-
+from django.utils.safestring import SafeText
 from dateutil import parser
 
 register = template.Library()
 
+import logging
+logger = logging.getLogger('django.request')
+
 
 @register.filter(name='to_date')
 def to_date(value):
-    if type(value) in [str, unicode]:
+    logger.info('to_date: %s %s' % (value, type(value)))
+    if type(value) in [SafeText, str, unicode]:
         return parser.parse(value)
     return value
