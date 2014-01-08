70ea26831fb27319278b32ff4fbbd771b265537f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-08 14:15:56 +0100

    Added test for 83b delivered email

--- toolkit/apps/eightythreeb/tests/__init__.py
+++ toolkit/apps/eightythreeb/tests/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from test_attachments import *
-from test_usps import *
-from test_trackingcode import *
-from test_markers import *
+from .test_attachments import *
+from .test_usps import *
+from .test_trackingcode import *
+from .test_markers import *
 
-from test_reminder_email import *
+from .test_reminder_email import *
+from .test_usps_track_response_email import *
