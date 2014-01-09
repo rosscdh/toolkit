7ed14d1925bfcb5bc33c088b59f5f8af66cb3c5b
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-09 13:59:42 +0100

    improved tests fro USPS field validation

--- toolkit/apps/eightythreeb/tests/test_usps_trackingcode_django_validators.py
+++ toolkit/apps/eightythreeb/tests/test_usps_trackingcode_django_validators.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from usps.validators import USPSTrackingCodeField
+from usps.validators import USPSTrackingCodeField, InvalidTrackingNumber
 
 
 class BaseTrackingCode(unittest.TestCase):
@@ -21,11 +21,22 @@ class Django_USS128_TrackingCodeTest(BaseTrackingCode):
     subject = USPSTrackingCodeField
     value = '70132630000013657033'
 
+    def test_invalid(self):
+        value = 'this_is_wrong'
+        subject = self.subject()
+        with self.assertRaises(InvalidTrackingNumber):
+            subject.clean(value=value)
+
 
 class Django_USS39_TrackingCodeTest(BaseTrackingCode):
     subject = USPSTrackingCodeField
     value = 'EJ958083578US'
 
+    def test_invalid(self):
+        value = 'this_is_wrong'
+        subject = self.subject()
+        with self.assertRaises(InvalidTrackingNumber):
+            subject.clean(value=value)
 
 if __name__ == '__main__':
     unittest.main()
