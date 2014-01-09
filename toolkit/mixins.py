8cf584fc8f159d8ee0478b8540911118f84f3b10
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-09 14:09:37 +0100

    Added AjaxableFormViewResponseMixin

--- toolkit/mixins.py
+++ toolkit/mixins.py
@@ -31,3 +31,38 @@ class ModalForm(BaseForm):
 
 class ModalView(View):
     template_name = 'modal.html'
+
+
+class AjaxableFormViewResponseMixin(object):
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    and Modal Views
+    """
+    def render_to_json_response(self, context, **response_kwargs):
+        data = json.dumps(context)
+        response_kwargs['content_type'] = 'application/json'
+        return HttpResponse(data, **response_kwargs)
+
+    def form_invalid(self, form):
+        if self.request.is_ajax():
+            errors = form.errors['__all__'] if '__all__' in form.errors else form.errors
+            data = {
+                'errors': errors
+            }
+            return self.render_to_json_response(data, status=400)
+        else:
+            return super(AjaxableResponseMixin, self).form_invalid(form)
+
+    def form_valid(self, form):
+        """ save the form but also render via ajax if ajax request """
+        if self.request.is_ajax():
+            if hasattr(form, 'instance'):
+                form.instance.save()
+                data = {
+                    'pk': form.instance.pk,
+                    'url': form.instance.get_absolute_url() if hasattr(form.instance, 'get_absolute_url') else None,
+                }
+                return self.render_to_json_response(data)
+
+        return super(AjaxableResponseMixin, self).form_valid(form)
