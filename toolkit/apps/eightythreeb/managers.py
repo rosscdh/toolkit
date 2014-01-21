b7a4faa1a0474551f442bfb2021052889d272e32
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-21 12:19:44 +0100

    added back the filing_date filter on the managers

--- toolkit/apps/eightythreeb/managers.py
+++ toolkit/apps/eightythreeb/managers.py
@@ -21,7 +21,8 @@ class EightyThreeBManager(models.Manager):
     Default objects manager with helper methods
     """
     def mail_delivery_pending(self):
-        return super(EightyThreeBManager, self).get_query_set().filter(status=self.model.STATUS.irs_recieved)
+        return super(EightyThreeBManager, self).get_query_set().filter(status=self.model.STATUS.irs_recieved) \
+                                                               .filter(filing_date__gte=datetime.date.today())
 
     def incomplete(self):
         """
@@ -29,8 +30,8 @@ class EightyThreeBManager(models.Manager):
         exclude completed as well as those in the waiting for usps delivery as it is out of the users hands at this point
         """
         return super(EightyThreeBManager, self).get_query_set() \
-                                                .exclude(status__in=self.model.INCOMPLETE_EXCLUDED_STATUS)
-                                                #.filter(filing_date__gte=datetime.date.today())
+                                                .exclude(status__in=self.model.INCOMPLETE_EXCLUDED_STATUS) \
+                                                .filter(filing_date__gte=datetime.date.today())
 
 
 class AttachmentManger(models.Manager):
