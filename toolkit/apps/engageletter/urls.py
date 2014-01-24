11f30d969802487b1c8468f96cea1dee6183978e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-24 14:07:35 +0100

    made url namespace nicer

--- toolkit/apps/engageletter/urls.py
+++ toolkit/apps/engageletter/urls.py
@@ -6,6 +6,6 @@ from .views import SignAndSendEngagementLetterView, SetupEngagementLetterView
 
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[\w-]+)/lawyer/template/$', login_required(SetupEngagementLetterView.as_view()), name='lawyer_template'),
+    url(r'^(?P<slug>[\w-]+)/template/lawyer/$', login_required(SetupEngagementLetterView.as_view()), name='lawyer_template'),
     url(r'^(?P<slug>[\w-]+)/sign/$', login_required(SignAndSendEngagementLetterView.as_view()), name='sign'),
 )
