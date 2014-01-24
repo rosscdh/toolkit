aacd391b24d4d3a3b2d3e493c8836e1761fd8544
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-24 14:04:10 +0100

    Added engagement letter text

--- toolkit/apps/me/templatetags/me_tags.py
+++ toolkit/apps/me/templatetags/me_tags.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger('django.request')
 
 
-@register.inclusion_tag('engageletter/partials/lawyer_header.html', takes_context=False)
+@register.inclusion_tag('lawyer/letterhead/lawyer_header.html', takes_context=False)
 def lawyer_header(lawyer):
     return {
         'lawyer': lawyer,
@@ -17,7 +17,7 @@ def lawyer_header(lawyer):
     }
 
 
-@register.inclusion_tag('engageletter/partials/lawyer_footer.html', takes_context=False)
+@register.inclusion_tag('lawyer/letterhead/lawyer_footer.html', takes_context=False)
 def lawyer_footer(lawyer):
     return {
         'lawyer': lawyer,
