8237637ad6bbd33ae575d93b02bdabcc9ff4e957
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-24 14:31:15 +0100

    refactored workspace.views into a module so that we can access the objects neatly

--- toolkit/apps/workspace/views/tool_objects.py
+++ toolkit/apps/workspace/views/tool_objects.py
@@ -4,97 +4,22 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.template.loader import get_template
 from django.template.defaultfilters import slugify
-from django.template import Context, TemplateDoesNotExist
-from django.http import HttpResponse, HttpResponseRedirect
+from django.template import TemplateDoesNotExist
+from django.http import HttpResponse
 
-from django.views.generic import (FormView,
-                                  ListView,
+from django.views.generic import (ListView,
                                   CreateView,
                                   UpdateView,
                                   DetailView)
 
-from toolkit.mixins import AjaxFormView, AjaxModelFormView, ModalView
-
-from .models import Workspace, Tool, InviteKey
-from .forms import WorkspaceForm, AddWorkspaceTeamMemberForm, InviteUserForm
-from .mixins import WorkspaceToolViewMixin, WorkspaceToolFormViewMixin, IssueSignalsMixin
-
-from .services import PDFKitService  # , HTMLtoPDForPNGService
+from ..models import Workspace, Tool, InviteKey
+from ..forms import InviteUserForm
+from ..mixins import WorkspaceToolViewMixin, WorkspaceToolFormViewMixin, IssueSignalsMixin
+from ..services import PDFKitService  # , HTMLtoPDForPNGService
 
 import datetime
 import logging
 logger = logging.getLogger('django.request')
-
-
-class AddUserToWorkspace(ModalView, AjaxFormView, FormView):
-    form_class = AddWorkspaceTeamMemberForm
-
-    def dispatch(self, request, *args, **kwargs):
-        self.workspace = get_object_or_404(Workspace, slug=self.kwargs.get('slug'))
-        return super(AddUserToWorkspace, self).dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super(AddUserToWorkspace, self).get_context_data(**kwargs)
-        context.update({
-            'workspace': self.workspace,
-        })
-        return context
-
-    def get_form_kwargs(self):
-        kwargs = super(AddUserToWorkspace, self).get_form_kwargs()
-        kwargs.update({
-            'workspace': self.workspace
-        })
-        return kwargs
-
-    def get_success_url(self):
-        return reverse('workspace:view', kwargs={'slug': self.workspace.slug})
-
-    def form_valid(self, form):
-        # save the form
-        user, is_new = form.save()
-        self.workspace.participants.add(user)
-
-        if is_new is True:
-            messages.success(self.request, 'You have sucessfully added a new user "%s" to the workspace' % user)
-        else:
-            messages.success(self.request, 'You have sucessfully added "%s" to the workspace' % user)
-
-        return super(AddUserToWorkspace, self).form_valid(form)
-
-
-class CreateWorkspaceView(ModalView, AjaxModelFormView, CreateView):
-    form_class = WorkspaceForm
-
-    def dispatch(self, request, *args, **kwargs):
-        # ensure that only lawyers can create
-        if request.user.profile.is_lawyer is False:
-            messages.error(request, 'Sorry, you must be an Attorney to access this')
-            return HttpResponseRedirect(reverse('dash:default'))
-
-        return super(CreateWorkspaceView, self).dispatch(request, *args, **kwargs)
-
-    def get_success_url(self):
-        return self.object.get_absolute_url()
-
-    def form_valid(self, form):
-        response = super(CreateWorkspaceView, self).form_valid(form)
-
-        # add lawyer
-        self.object.lawyer = self.request.user
-        self.object.save(update_fields=['lawyer'])
-
-        # add user as participant
-        self.object.participants.add(self.request.user)
-
-        # @BUSINESS_RULE - 83(b) is added by default
-        # add the 83b tool by default
-        tool_83b = Tool.objects.get(slug='83b-election-letters')
-        self.object.tools.add(tool_83b)
-
-        messages.success(self.request, 'You have sucessfully created a new workspace')
-
-        return response
 
 
 class WorkspaceToolObjectsListView(WorkspaceToolViewMixin, ListView):
@@ -110,7 +35,6 @@ class WorkspaceToolObjectsListView(WorkspaceToolViewMixin, ListView):
 
         if self.tool.markers.current_marker.get_action_url() is not None:
             create_url = self.tool.markers.current_marker.get_action_url()
-
 
         context.update({
             # if there are no tool.userclass_that_can_create defined then anyone can create
