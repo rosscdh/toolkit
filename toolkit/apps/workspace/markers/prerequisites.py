8e18fef9f27ab178490e0579747cdef057a1e922
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-27 16:38:01 +0100

    added missing properties

--- toolkit/apps/workspace/markers/prerequisites.py
+++ toolkit/apps/workspace/markers/prerequisites.py
@@ -10,6 +10,7 @@ class Prerequisite(Marker):
     p.action = None  # this means the prerequisite has been met and we can hide the marker
     p.action = /url/to/place/  # this means the prerequisite has not been met
     """
+    val = 0
     is_prerequisite = True
     hide_when_complete = True
-
+    workspace = None  # required so we can evaluate without tool which we dont have on tool.create
