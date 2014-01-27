62ee30ebe1fd2f744f9dca3676fed181f3a19388
Jamie Connolly <jamie@jamieconnolly.com>
2014-01-27 16:40:24 +0000

    Update the lawyer view of the engagement letter (hides rate info temporarily)

--- toolkit/apps/engageletter/forms.py
+++ toolkit/apps/engageletter/forms.py
@@ -2,8 +2,9 @@
 from django import forms
 from django.core.urlresolvers import reverse
 
+from crispy_forms.bootstrap import FieldWithButtons, StrictButton
 from crispy_forms.helper import FormHelper, Layout
-from crispy_forms.layout import ButtonHolder, Submit, Div, HTML
+from crispy_forms.layout import ButtonHolder, Div, HTML, Submit
 
 from parsley.decorators import parsleyfy
 
@@ -20,7 +21,7 @@ logger = logging.getLogger('django.request')
 class BaseForm(WorkspaceToolFormMixin):
     address1 = forms.CharField(
         error_messages={
-            'required': "Address can't be blank."
+            'required': "Address can not be blank."
         },
         label='',
         required=True,
@@ -35,7 +36,7 @@ class BaseForm(WorkspaceToolFormMixin):
 
     city = forms.CharField(
         error_messages={
-            'required': "City can't be blank."
+            'required': "City can not be blank."
         },
         label='',
         required=True,
@@ -45,7 +46,7 @@ class BaseForm(WorkspaceToolFormMixin):
     state = forms.ChoiceField(
         choices=USPS_CHOICES,
         error_messages={
-            'required': "State can't be blank."
+            'required': "State can not be blank."
         },
         label='',
         help_text='',
@@ -55,7 +56,7 @@ class BaseForm(WorkspaceToolFormMixin):
 
     post_code = USZipCodeField(
         error_messages={
-            'required': "Zip code can't be blank."
+            'required': "Zip code can not be blank."
         },
         label='',
         required=True,
@@ -67,7 +68,6 @@ class BaseForm(WorkspaceToolFormMixin):
         super(BaseForm, self).__init__(*args, **kwargs)
 
     def save(self):
-
         if self.instance is not None:
             # use the currently associated user
             user = self.instance.user
@@ -75,8 +75,8 @@ class BaseForm(WorkspaceToolFormMixin):
 
         else:
             # Ensure we have a customer with this info
-            customer_service = EnsureCustomerService(email=self.cleaned_data.get('client_email'),
-                                                     full_name=self.cleaned_data.get('client_full_name'))
+            customer_service = EnsureCustomerService(email=self.cleaned_data.get('signatory_email'),
+                                                     full_name=self.cleaned_data.get('signatory_full_name'))
             customer_service.process()
             user = customer_service.user
             is_new = customer_service.is_new
@@ -94,6 +94,7 @@ class BaseForm(WorkspaceToolFormMixin):
 @parsleyfy
 class LawyerForm(BaseForm):
     date_of_property_transfer = forms.DateField(
+        help_text='',
         input_formats=['%B %d, %Y', '%Y-%m-%d %H:%M:%S'],
         label='Date of letter',
         widget=forms.DateInput(
@@ -104,9 +105,10 @@ class LawyerForm(BaseForm):
             format='%B %d, %Y'
         )
     )
-    client_full_name = forms.CharField(
+
+    signatory_full_name = forms.CharField(
         error_messages={
-            'required': "Client name can't be blank."
+            'required': "Signatory name can not be blank."
         },
         help_text='',
         label='Full name',
@@ -114,10 +116,10 @@ class LawyerForm(BaseForm):
         widget=forms.TextInput(attrs={'placeholder': 'John Smith', 'size': '40'})
     )
 
-    client_email = forms.EmailField(
+    signatory_email = forms.EmailField(
         error_messages={
-            'invalid': "Client email is invalid.",
-            'required': "Client email can't be blank."
+            'invalid': "Signatory email is invalid.",
+            'required': "Signatory email can not be blank."
         },
         help_text='',
         label='Email address',
@@ -125,54 +127,120 @@ class LawyerForm(BaseForm):
         widget=forms.EmailInput(attrs={'placeholder': 'john@acmeinc.com', 'size': '40'})
     )
 
-    signatory_full_name = forms.CharField(
-        label='Signatory name',
-        required=True,
-        widget=forms.TextInput(attrs={'placeholder': 'John Smith', 'size': '40'})
-    )
     signatory_title = forms.CharField(
+        error_messages={
+            'required': "Signatory title can not be blank."
+        },
         label='Signatory title',
+        help_text='',
         required=True,
         widget=forms.TextInput(attrs={'placeholder': 'CEO', 'size': '40'})
     )
 
-    file_number = forms.CharField()
-    rate_hourly_from = forms.DecimalField(max_digits=10, decimal_places=2)
-    rate_hourly_to = forms.DecimalField(max_digits=10, decimal_places=2)
-    rate_hourly_increments = forms.IntegerField()
-    rate_flat_fee = forms.DecimalField(max_digits=10, decimal_places=2)
+    company_name = forms.CharField(
+        error_messages={
+            'required': "Company name can not be blank."
+        },
+        label='Company name',
+        help_text='',
+        required=True,
+        widget=forms.TextInput(attrs={'placeholder': 'Acme Inc', 'size': '40'})
+    )
+
+    # file_number = forms.CharField()
+    # rate_hourly_from = forms.DecimalField(max_digits=10, decimal_places=2)
+    # rate_hourly_to = forms.DecimalField(max_digits=10, decimal_places=2)
+    # rate_hourly_increments = forms.IntegerField()
+    # rate_flat_fee = forms.DecimalField(max_digits=10, decimal_places=2)
 
     legal_services = forms.CharField(
-        label='Description of engagement',
+        error_messages={
+            'required': "Legal services can not be blank."
+        },
+        help_text='',
+        label='Legal services',
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={
+            'cols': '80',
+            'data-toggle': 'summernote'
+        })
     )
+
     service_description = forms.CharField(
+        error_messages={
+            'required': "Engagement description can not be blank."
+        },
+        help_text='',
         label='Description of engagement',
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={
+            'cols': '80',
+            'data-toggle': 'summernote'
+        })
     )
+
     fees = forms.CharField(
+        error_messages={
+            'required': "Fees can not be blank."
+        },
+        help_text='',
         label='Fees',
         required=True,
-        widget=forms.Textarea
+        widget=forms.Textarea(attrs={
+            'cols': '80',
+            'data-toggle': 'summernote'
+        })
     )
 
     def __init__(self, *args, **kwargs):
         super(LawyerForm, self).__init__(*args, **kwargs)
+
+        # change the required state on some fields
+        self.fields['address1'].required = False
+        self.fields['address2'].required = False
+        self.fields['city'].required = False
+        self.fields['state'].required = False
+        self.fields['post_code'].required = False
+
+        # set the readonly fields
+        self.fields['address1'].widget.attrs['readonly'] = 'readonly'
+        self.fields['address2'].widget.attrs['readonly'] = 'readonly'
+        self.fields['city'].widget.attrs['readonly'] = 'readonly'
+        self.fields['state'].widget.attrs['disabled'] = 'disabled'
+        self.fields['post_code'].widget.attrs['readonly'] = 'readonly'
+
         self.helper.layout = Layout(
-            'date_of_property_transfer',
-            'client_full_name',
-            'client_email',
-            'signatory_full_name',
-            'signatory_title',
+            HTML('{% include "partials/form-errors.html" with form=form %}'),
+            Div(
+                HTML('<legend>Signatory details</legend>'),
+                Div(
+                    'signatory_full_name',
+                    'signatory_email',
+                    css_class='form-inline'
+                ),
+                Div(
+                    'signatory_title',
+                    'company_name',
+                    css_class='form-inline'
+                ),
+            ),
+            Div(
+                HTML('<legend>Engagement Letter Information</legend>'),
+                FieldWithButtons(
+                    'date_of_property_transfer',
+                    StrictButton('<span class="fui-calendar"></span>'),
+                    css_class='datetime'
+                ),
+                'legal_services',
+                'service_description',
+                'fees',
 
-            'file_number',
-            'rate_hourly_from',
-            'rate_hourly_to',
-            'rate_hourly_increments',
-            'rate_flat_fee',
-
+                # 'file_number',
+                # 'rate_hourly_from',
+                # 'rate_hourly_to',
+                # 'rate_hourly_increments',
+                # 'rate_flat_fee',
+            ),
             Div(
                 HTML('<legend>Additional details (Client to complete)</legend>'),
                 'address1',
@@ -185,11 +253,8 @@ class LawyerForm(BaseForm):
                 ),
                 css_class='dialog dialog-info form-dialog'
             ),
-
-            'description',
-            'fees',
             ButtonHolder(
-                Submit('submit', 'Continue', css_class='btn-hg btn-primary'),
+                Submit('submit', 'Continue', css_class='btn btn-primary btn-lg btn-wide'),
                 css_class='form-group'
             )
         )
@@ -203,7 +268,6 @@ class LawyerForm(BaseForm):
 
 @parsleyfy
 class CustomerForm(BaseForm):
-
     def __init__(self, *args, **kwargs):
         super(CustomerForm, self).__init__(*args, **kwargs)
         self.helper.layout = Layout(
