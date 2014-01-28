b7b8c2ea484a8fab4933a9da93d5bab1e020330e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-28 12:34:40 +0100

    made file accessible for testing

--- toolkit/apps/workspace/services/word.py
+++ toolkit/apps/workspace/services/word.py
@@ -10,18 +10,20 @@ from . import logger
 
 
 class PandocDocxService(BasePandocService):
+    docx_file = None
+
     def generate(self, html, **kwargs):
         from_format = kwargs.get('from_format', 'html')
         to_format = kwargs.get('to_format', 'docx')
         # create temp file
-        docx_file = NamedTemporaryFile(suffix='.docx')
+        self.docx_file = NamedTemporaryFile(suffix='.docx')
 
-        logger.info('Created .docx file at: %s' % (docx_file.name, ))
+        logger.info('Created .docx file at: %s' % (self.docx_file.name, ))
 
         # generate it using pandoc
-        self.service.convert(html, to_format, format=from_format, extra_args=('--output', docx_file.name))
+        self.service.convert(html, to_format, format=from_format, extra_args=('--output', self.docx_file.name))
         # return the file which is now populated with the docx forms
-        return docx_file
+        return self.docx_file
 
 
 class WordService(PandocDocxService):
