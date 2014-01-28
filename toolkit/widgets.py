ae20e8ff227081367a1ef0b94c1e5c519bf9192e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-28 09:43:24 +0100

    Added summer note field and widget

---
+++ toolkit/widgets.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from django.forms.widgets import Textarea
+
+
+class SummerNoteWidget(Textarea):
+    custom_attrs = {
+        'data-toggle': 'summernote',
+        'cols': '100',
+    }
+
+    class Media:
+        css = {
+            'all': ('css/summernote.css',),
+        }
+        js = ('js/summernote.min.js', 'js/tool-form.js',)
+
+    def __init__(self, *args, **kwargs):
+        self.custom_attrs.update(kwargs.get('attrs', {}))
+        attrs = self.custom_attrs.copy()
+        super(SummerNoteWidget, self).__init__(attrs=attrs, *args, **kwargs)
