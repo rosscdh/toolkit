13cde2eb962f3f1c9007fae9c3997e5179ab6be1
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-29 11:11:10 +0100

    Added pdf service and added standalone and smart to pandoc

--- toolkit/apps/workspace/services/word.py
+++ toolkit/apps/workspace/services/word.py
@@ -9,6 +9,28 @@ from .pandoc import BasePandocService
 from . import logger
 
 
+class PandocPDFService(BasePandocService):
+    docx_file = None
+
+    def generate(self, html, **kwargs):
+        from_format = kwargs.get('from_format', 'html')
+        to_format = kwargs.get('to_format', 'tex')
+        # create temp file
+        self.docx_file = NamedTemporaryFile(suffix='.pdf')
+
+        logger.info('Created .docx file at: %s' % (self.docx_file.name, ))
+
+        extra_args = (
+            '--smart',
+            '--standalone',
+            '-o', self.docx_file.name
+        )
+        # generate it using pandoc
+        self.service.convert(html, to_format, format=from_format, extra_args=extra_args)
+        # return the file which is now populated with the docx forms
+        return self.docx_file
+
+
 class PandocDocxService(BasePandocService):
     docx_file = None
 
@@ -20,11 +42,15 @@ class PandocDocxService(BasePandocService):
 
         logger.info('Created .docx file at: %s' % (self.docx_file.name, ))
 
+        extra_args = (
+            '--smart',
+            '--standalone',
+            '-o', self.docx_file.name
+        )
         # generate it using pandoc
-        self.service.convert(html, to_format, format=from_format, extra_args=('--output', self.docx_file.name))
+        self.service.convert(html, to_format, format=from_format, extra_args=extra_args)
         # return the file which is now populated with the docx forms
         return self.docx_file
-
 
 class WordService(PandocDocxService):
     """
