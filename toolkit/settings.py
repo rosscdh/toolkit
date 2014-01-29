23e9e7d86f37bc884b4617084bd0e744acebb5e7
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-29 15:15:18 +0100

    Added HS settings

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -199,6 +199,10 @@ AWS_HEADERS = {
     'x-amz-acl': 'public-read',
 }
 
+HELLOSIGN_AUTHENTICATION = ("founders@lawpal.com", "test2007")
+HELLOSIGN_CLIENT_ID = '9bc892af173754698e3fa30dedee3826'
+HELLOSIGN_CLIENT_SECRET = '8d770244b9971abfe789f5224552239d'
+
 REST_FRAMEWORK = {
     # Use hyperlinked styles by default.
     'DEFAULT_RENDERER_CLASSES': (
