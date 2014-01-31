3901259451e417c23117140ea0c2dad096235b4e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-31 14:28:36 +0100

    added helper tag for full url in templates

--- toolkit/apps/default/templatetags/toolkit_tags.py
+++ toolkit/apps/default/templatetags/toolkit_tags.py
@@ -13,6 +13,12 @@ _DOMAIN_WITHOUT_END_SLASH = _CURRENT_SITE.domain[0:-1] if _CURRENT_SITE.domain[-
 
 
 @register.simple_tag
+def FULL_DOMAIN_PATH(path=None):
+    path = path if path is not None else '/'
+    return '{domain}{path}'.format(domain=_DOMAIN_WITHOUT_END_SLASH, path=path)
+FULL_DOMAIN_PATH.is_safe = True
+
+@register.simple_tag
 def ABSOLUTE_STATIC_URL(path=None):
     if path is not None:
         path = path if settings.STATIC_URL in path else '%s%s' % (settings.STATIC_URL, path)
