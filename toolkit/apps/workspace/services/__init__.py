f01511d2922883f65cb433ca1037c17c271ded8e
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-31 12:07:15 +0100

    Added working Hellosign management command needing feedback form HS on why its not working tho

--- toolkit/apps/workspace/services/__init__.py
+++ toolkit/apps/workspace/services/__init__.py
@@ -9,4 +9,5 @@ logger = logging.getLogger('django.request')
 from .customer import EnsureCustomerService
 from .pdf import PDFKitService
 from .word import WordService
+from .hellosign_service import HelloSignService
 from .tracker import USPSTrackingService, USPSResponse
