6424ed558319241a99df0c98409391d41157419a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-01-31 13:08:33 +0100

    Updated test script to accept multiple slugs

--- toolkit/apps/workspace/management/commands/hellosign_engageletter.py
+++ toolkit/apps/workspace/management/commands/hellosign_engageletter.py
@@ -17,29 +17,38 @@ class Command(BaseCommand):
     args = '<engageletter slug ...>'
     help = "Test integration with HelloSign"
 
+    slugs = []
     service = HelloSignService
 
+    @property
+    def records(self):
+        return EngagementLetter.objects.filter(slug__in=self.slugs)
+
     def handle(self, *args, **options):
+        self.slugs = args
+
         try:
-            engageletter_slug = args[0]
+            args[0]
         except IndexError:
             raise CommandError('You must specify a engageletter slug')
 
-        e = EngagementLetter.objects.get(slug=engageletter_slug)
+        for e in self.records:
 
-        invitees = [
-            {'name': 'Ross Customer', 'email': 'ross+customer@lawpal.com'},
-            {'name': 'Ross Tech Lawyer', 'email': 'ross+lawyer@lawpal.com'}
-        ]
+            # invitees = [
+            #     {'name': 'Ross Customer', 'email': 'ross+customer@lawpal.com'},
+            #     {'name': 'Ross Tech Lawyer', 'email': 'ross+lawyer@lawpal.com'}
+            # ]
 
-        subject = 'Test Signing'
-        message = 'This is a test singing please delete'
+            # subject = 'Test Signing'
+            # message = 'This is a test singing please delete'
 
-        doc_service = WordService()
-        document = doc_service.generate(html=e.html())
+            # doc_service = WordService()
+            # document = doc_service.generate(html=e.html())
 
-        service = self.service(document=document, invitees=invitees, subject=subject, message=message)
-        result = service.send_for_signing(test_mode=1, client_id=settings.HELLOSIGN_CLIENT_ID)
-        PPP.pprint(result.json())
+            # service = self.service(document=document, invitees=invitees, subject=subject, message=message)
+            # result = service.send_for_signing(test_mode=1, client_id=settings.HELLOSIGN_CLIENT_ID)
+
+            result = e.send_for_signing()
+            PPP.pprint(result.json())
         import pdb;pdb.set_trace()
-        
+            
