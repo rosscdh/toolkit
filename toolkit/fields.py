a1f394f6612c83a59a67afc98f618905ba31e66d
Jamie Connolly <jamie@jamieconnolly.com>
2014-02-04 12:43:43 +0000

    Try and strip out anything that doesn't come from Summernote

--- toolkit/fields.py
+++ toolkit/fields.py
@@ -1,8 +1,50 @@
 # -*- coding: utf-8 -*-
 from django.forms import CharField
 
+import bleach
+
 from .widgets import SummernoteWidget
 
 
 class SummernoteField(CharField):
+    attributes = {
+        'blockquote': ['style',],
+        'div': ['style',],
+        'span': ['style',],
+    }
+
+    styles = [
+        'border',
+        'font-style',
+        'font-weight',
+        'margin',
+        'padding',
+        'text-align',
+        'text-decoration',
+    ]
+
+    tags = [
+        'blockquote',
+        'br',
+        'div',
+        'li',
+        'ol',
+        'span',
+        'ul',
+    ]
+
     widget = SummernoteWidget
+
+    def __init__(self, tags=None, attributes=None, styles=None, strip=True, *args, **kwargs):
+        super(SummernoteField, self).__init__(*args, **kwargs)
+
+        self.strip = strip
+        if tags is not None:
+            self.tags = tags
+        if attributes is not None:
+            self.attributes = attributes
+        if styles is not None:
+            self.styles = styles
+
+    def to_python(self, value):
+        return bleach.clean(value, tags=self.tags, attributes=self.attributes, styles=self.styles, strip=self.strip)
