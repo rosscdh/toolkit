344e9037cd7d0ccf2c1b8a4052fb506af0b0cfbf
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-17 16:39:44 +0100

    api HyperlinkedModelField now working. Don't use namespace

--- toolkit/api/serializers/user.py
+++ toolkit/api/serializers/user.py
@@ -3,11 +3,13 @@
 Items are either todo items or document items
 """
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        exclude = ('pk', 'password', 'is_superuser', 'is_staff', 'groups', 'user_permissions')
+        lookup_field = 'username'
+        fields = ('id', 'url', 'last_login', 'username', 'first_name', 'last_name', 'email', 'is_active', 'date_joined',)
