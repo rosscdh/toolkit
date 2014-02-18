248fbfa76e691a12748815fa3add1bded87f6821
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-18 15:24:56 +0100

    Adding attachment model methods

--- toolkit/core/attachment/models.py
+++ toolkit/core/attachment/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.template.defaultfilters import slugify
+from storages.backends.s3boto import S3BotoStorage
 
 from jsonfield import JSONField
 
@@ -14,12 +15,18 @@ def _upload_file(instance, filename):
 
 
 class Attachment(models.Model):
-    attachment = models.FileField(upload_to=_upload_file)
+    attachment = models.FileField(upload_to=_upload_file, storage=S3BotoStorage())
     uploaded_by = models.ForeignKey('auth.User')
 
-    revisions = models.ManyToManyField('attachment.Attachment', null=True, blank=True, symmetrical=False, related_name="attachment_revisions")
+    revisions = models.ManyToManyField('attachment.Attachment', null=True, blank=True, symmetrical=False, related_name="own_revisions")
 
     data = JSONField(default={}, blank=True)
 
     date_created = models.DateTimeField(auto_now=False, auto_now_add=True, db_index=True)
     date_modified = models.DateTimeField(auto_now=True, auto_now_add=True, db_index=True)
+
+    def next(self):
+        return self.revisions.filter(pk__gt=self.pk)
+
+    def previous(self):
+        return self.revisions.filter(pk__lt=self.pk)
