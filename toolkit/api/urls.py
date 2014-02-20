62901d30d50928dc58ca8e612bf8eb55fb850748
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-20 16:00:05 +0100

    Client Endpoints

--- toolkit/api/urls.py
+++ toolkit/api/urls.py
@@ -12,18 +12,21 @@ from .views import RevisionEndpoint
 #from .views import WorkflowEndpoint
 
 router = routers.SimpleRouter()
-router.register(r'account', AccountEndpoint)
 router.register(r'users', UserEndpoint)
+router.register(r'account', AccountEndpoint)
+
 router.register(r'matters', MatterEndpoint)
+router.register(r'clients', ClientEndpoint)
 router.register(r'items', ItemEndpoint)
 router.register(r'revisions', RevisionEndpoint)
 
+
 urlpatterns = router.urls + patterns('',
     #url(r'^account/', AccountEndpoint.as_view(), name='account'),
-    url(r'^clients/', ClientEndpoint.as_view(), name='clients'),
-#    url(r'^matters/', MatterEndpoint.as_view(), name='matters'),
-#    url(r'^items/', ItemEndpoint.as_view(), name='items'),
-#    url(r'^attachments/', AttachmentEndpoint.as_view(), name='attachments'),
-#        url(r'^attachments/(?P<slug>(.*))/revisions/(?P<revision>(\d+))/', AttachmentEndpoint.as_view(), name='attachment_revision'),
-#    url(r'^workflows/', WorkflowEndpoint.as_view(), name='workflows'),
+    #url(r'^clients/', ClientEndpoint.as_view(), name='clients'),
+    #url(r'^matters/', MatterEndpoint.as_view(), name='matters'),
+    #url(r'^items/', ItemEndpoint.as_view(), name='items'),
+    #url(r'^attachments/', AttachmentEndpoint.as_view(), name='attachments'),
+    #    url(r'^attachments/(?P<slug>(.*))/revisions/(?P<revision>(\d+))/', AttachmentEndpoint.as_view(), name='attachment_revision'),
+    #url(r'^workflows/', WorkflowEndpoint.as_view(), name='workflows'),
 )
