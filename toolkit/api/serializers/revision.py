65fa5e8b3d802fe8e4d1fb865e2dbb7b65bdfaab
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-21 13:50:15 +0100

    revisión serializer up to speed

--- toolkit/api/serializers/revision.py
+++ toolkit/api/serializers/revision.py
@@ -5,6 +5,14 @@ from toolkit.core.attachment.models import Revision
 
 
 class RevisionSerializer(serializers.ModelSerializer):
+    revision_id = serializers.CharField(source='revision_id', read_only=True)
+    uploaded_by = serializers.HyperlinkedRelatedField(many=False, view_name='user-detail', lookup_field='username', read_only=True)
+    item = serializers.HyperlinkedRelatedField(many=False, view_name='item-detail')
+
+    reviewers = serializers.HyperlinkedRelatedField(many=True, view_name='user-detail', lookup_field='username')
+    signatories = serializers.HyperlinkedRelatedField(many=True, view_name='user-detail', lookup_field='username')
+
+
     class Meta:
         model = Revision
-        #exclude = ()
+        exclude = ('id', 'data', 'revisions')
