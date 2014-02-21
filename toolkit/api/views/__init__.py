c94b92b3c878980f5657afa01c7f8499812de8bb
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-21 13:52:10 +0100

    installing endpoints into urls

--- toolkit/api/views/__init__.py
+++ toolkit/api/views/__init__.py
@@ -2,7 +2,14 @@
 from .user import UserEndpoint
 from .account import AccountEndpoint
 from .client import ClientEndpoint
-from .matter import (MatterEndpoint, MatterItemsView, MatterItemView)
+from .matter import (MatterEndpoint,
+                     MatterItemsView,
+                     MatterItemView,
+                     ItemCurrentRevisionView as MatterItemCurrentRevisionView,
+                     ItemRevisionReviewerView,
+                     ItemRevisionSignatoryView,
+                     RemindReviewers,
+                     RemindSignatories)
 from .item import ItemEndpoint
 from .revision import RevisionEndpoint
 # from .workflow import WorkflowEndpoint
