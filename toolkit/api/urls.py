456123aa540744d2d4ef9291589105ef7edf8ac7
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-21 13:52:10 +0100

    installing endpoints into urls

--- toolkit/api/urls.py
+++ toolkit/api/urls.py
@@ -6,7 +6,14 @@ from rest_framework import routers
 from .views import UserEndpoint
 from .views import AccountEndpoint
 from .views import ClientEndpoint
-from .views import (MatterEndpoint, MatterItemsView, MatterItemView)
+from .views import (MatterEndpoint,
+                    MatterItemsView,
+                    MatterItemView,
+                    MatterItemCurrentRevisionView,
+                    ItemRevisionReviewerView,
+                    ItemRevisionSignatoryView,
+                    RemindReviewers,
+                    RemindSignatories)
 from .views import ItemEndpoint
 from .views import RevisionEndpoint
 #from .views import WorkflowEndpoint
@@ -17,7 +24,7 @@ router = routers.SimpleRouter()
 ViewSets
 """
 router.register(r'users', UserEndpoint)
-router.register(r'account', AccountEndpoint)
+router.register(r'account', AccountEndpoint, base_name='account')
 
 router.register(r'matters', MatterEndpoint)
 router.register(r'clients', ClientEndpoint)
@@ -29,8 +36,14 @@ router.register(r'revisions', RevisionEndpoint)
 Generics
 """
 urlpatterns = router.urls + patterns('',
-
+    #
+    # Matter Items
+    #
     url(r'^matters/(?P<matter_slug>[\w-]+)/items/$', MatterItemsView.as_view(), name='matter_items'),
     url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/$', MatterItemView.as_view(), name='matter_item'),
-
+    url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/revision/$', MatterItemCurrentRevisionView.as_view(), name='matter_item_revision'),
+    url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/revision/reviewer/(?P<username>\w+)/$', ItemRevisionReviewerView.as_view(), name='item_revision_reviewer'),
+    url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/revision/signatory/(?P<username>\w+)/$', ItemRevisionSignatoryView.as_view(), name='item_revision_signatory'),
+    url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/revision/reviewers/remind/$', RemindReviewers.as_view(), name='item_revision_remind_reviewers'),
+    url(r'^matters/(?P<matter_slug>[\w-]+)/items/(?P<item_slug>[\d\w-]+)/revision/signatories/remind/$', RemindSignatories.as_view(), name='item_revision_remind_signatories'),
 )
