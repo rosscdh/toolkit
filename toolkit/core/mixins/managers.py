ea275e9bae4e5c05fe047f60e40e39d8241c0a8e
Jamie Connolly <jamie@jamieconnolly.com>
2014-02-24 17:52:15 +0000

    Show the deleted records in admin

--- toolkit/core/mixins/managers.py
+++ toolkit/core/mixins/managers.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
+from django.db.models.query import QuerySet
 
 from .query import IsDeletedQuerySet
 
@@ -10,6 +11,9 @@ class IsDeletedManager(models.Manager):
     """
     def get_queryset(self):
         return IsDeletedQuerySet(self.model, using=self._db).filter(is_deleted=False)
+
+    def all_with_deleted(self):
+        return QuerySet(self.model, using=self._db)
 
     def deleted(self):
         return super(IsDeletedManager, self).get_query_set().filter(is_deleted=True)
