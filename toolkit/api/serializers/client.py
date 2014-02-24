bdbf29aee76316bcf95ae01596f0d335a358e500
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-24 17:29:17 +0100

    Added serializer and slug null and blank for client

--- toolkit/api/serializers/client.py
+++ toolkit/api/serializers/client.py
@@ -5,5 +5,6 @@ from toolkit.core.client.models import Client
 
 
 class ClientSerializer(serializers.HyperlinkedModelSerializer):
+    lawyer = serializers.HyperlinkedRelatedField(many=False, view_name='user-detail', lookup_field='username')
     class Meta:
         model = Client
