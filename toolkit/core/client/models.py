0d3fa7c03050c5c1b38ccca91a028b640fd3b0e0
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-24 17:26:06 +0100

    added ensure client signal

--- toolkit/core/client/models.py
+++ toolkit/core/client/models.py
@@ -3,4 +3,8 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    lawyer = models.ForeignKey('auth.User')
+
+
+from .signals import (ensure_client_slug,)
