0260690d6b445b7477681e36bf4eabc75f65b391
Jamie Connolly <jamie@jamieconnolly.com>
2014-02-24 16:45:21 +0000

    Add in the IsDeletedQuerySet, IsDeletedManager and IsDeletedMixin (object)

---
+++ toolkit/core/mixins/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .managers import IsDeletedManager
+from .models import IsDeletedMixin
+from .query import IsDeletedQuerySet
---
+++ toolkit/core/mixins/query.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+from django.db.models.query import QuerySet
+
+
+class IsDeletedQuerySet(QuerySet):
+    """
+    Mixin to override the Queryset.delete() functionality of a manager
+    """
+    def delete(self, *args, **kwargs):
+        if 'is_deleted' in self.model._meta.get_all_field_names():
+            # should this call .delete on the model
+            self.update(is_deleted=True)
+        else:
+            super(IsDeletedQuerySet, self).delete()
