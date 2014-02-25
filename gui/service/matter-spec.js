f8a5f1eaea915eef1b7edabff69a6b5d28213547
leejsinclair <lee.j.sinclair@gmail.com>
2014-02-25 23:06:04 +1000

    Scaffhold for checklist angular JS app

---
+++ gui/service/matter-spec.js
@@ -0,0 +1,11 @@
+describe('matterService', function() {
+
+  beforeEach(module('toolkit-gui'));
+
+  it('should ...', inject(function(matter) {
+
+	//expect(matter.doSomething()).toEqual('something');
+
+  }));
+
+});
