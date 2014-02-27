333ddea00eb2a466d6c41b52419513f648d91308
Jamie Connolly <jamie@jamieconnolly.com>
2014-02-27 13:27:23 +0000

    Use the matter serializer in the templates

--- toolkit/apps/dashboard/views.py
+++ toolkit/apps/dashboard/views.py
@@ -1,5 +1,36 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from toolkit.api.serializers import MatterSerializer
+from toolkit.apps.workspace.models import Workspace
 
 
-class DashboardView(TemplateView):
+class DashboardView(ListView):
+    serializer_class = MatterSerializer
     template_name = 'dashboard/dashboard.html'
+
+    def get_queryset(self):
+        return Workspace.objects.mine(self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+
+        context.update({
+            'object_list': self.get_serializer(self.object_list, many=True).data
+        })
+
+        return context
+
+    def get_serializer(self, instance=None, data=None,
+                       files=None, many=False, partial=False):
+        """
+        Return the serializer instance that should be used for validating and
+        deserializing input, and for serializing output.
+        """
+        serializer_class = self.serializer_class
+        context = self.get_serializer_context()
+        return serializer_class(instance)
+
+    def get_serializer_context(self):
+        return {
+            'request': self.request
+        }
