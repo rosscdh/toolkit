795264c7d6976b7c463977ced2e53b2cee370f77
Jamie Connolly <jamie@jamieconnolly.com>
2014-02-27 16:42:01 +0000

    Modify some of the serializers to return proper data

--- toolkit/api/serializers/user.py
+++ toolkit/api/serializers/user.py
@@ -12,13 +12,16 @@ from toolkit.apps.default.models import UserProfile
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     #account = serializers.HyperlinkedRelatedField(source='profile', many=False, view_name='account-detail', lookup_field='username')
+    initials = serializers.SerializerMethodField('get_initials')
     user_class = serializers.SerializerMethodField('get_user_class')
 
     class Meta:
         model = User
         lookup_field = 'username'
-        fields = ('url', 'username', 'first_name',
-                  'last_name', 'email', 'is_active', 'user_class')
+        fields = ('url', 'first_name', 'last_name', 'initials', 'user_class')
+
+    def get_initials(self, obj):
+        return obj.get_initials()
 
     def get_user_class(self, obj):
         return obj.profile.user_class
