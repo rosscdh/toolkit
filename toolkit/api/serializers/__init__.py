469a1c622ad5551ba8322dcd8d35199898879675
Jamie Connolly <jamie@jamieconnolly.com>
2014-03-03 12:32:38 +0000

    Fix the errors with the tests and the API changes made

--- toolkit/api/serializers/__init__.py
+++ toolkit/api/serializers/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF-8 -*-
 from .hateoas import HATOAS
-from .user import UserSerializer
+
 from .account import AccountSerializer, PasswordSerializer
-from .client import ClientSerializer
+from .client import ClientSerializer, LiteClientSerializer
 from .matter import MatterSerializer, LiteMatterSerializer
 from .item import ItemSerializer
 from .revision import RevisionSerializer
+from .user import UserSerializer, LiteUserSerializer
 # from .workflow import WorkflowSerializer
--- toolkit/api/serializers/user.py
+++ toolkit/api/serializers/user.py
@@ -11,6 +11,9 @@ from toolkit.apps.default.models import UserProfile
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    The full representation of a User.
+    """
     #account = serializers.HyperlinkedRelatedField(source='profile', many=False, view_name='account-detail', lookup_field='username')
     initials = serializers.SerializerMethodField('get_initials')
     name = serializers.SerializerMethodField('get_full_name')
@@ -19,7 +22,6 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         lookup_field = 'username'
-        fields = ('url', 'first_name', 'last_name', 'name', 'initials', 'user_class')
 
     def get_full_name(self, obj):
         return obj.get_full_name()
@@ -29,3 +31,13 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     def get_user_class(self, obj):
         return obj.profile.user_class
+
+
+class LiteUserSerializer(UserSerializer):
+    """
+    The lite representation of a User.
+
+    Used when a user is referenced in other API objects.
+    """
+    class Meta(UserSerializer.Meta):
+        fields = ('first_name', 'initials', 'last_name', 'name', 'url', 'user_class')
