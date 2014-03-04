2becd6e18f869693c80960c41c6ba4c287c757f1
Ross Crawford <sendrossemail@gmail.com>
2014-03-04 22:05:53 +0100

    getting auth_nabckends and a protocol in place for authenticating

---
+++ toolkit/apps/review/auth_backends.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User, check_password
+
+from .models import ReviewDocument
+
+import logging
+logger = logging.getLogger('django.request')
+
+
+class ReviewDocumentBackend(object):
+    """
+    Authenticated based on the url
+    /review/:slug/:unique_key
+    where the slug gets the ReviewDocument
+    and the :unique_key provides a lookup in the ReviewDocument.data['auth']
+    which will provide the user pk
+    """
+    def authenticate(self, username=None, password=None):
+        # Check the username/password and return a User.
+        user = None
+        import pdb;pdb.set_trace()
+        try:
+
+            review = ReviewDocument.objects.get(slug=username)
+            pk = review.get_auth(key=password)
+            if pk is None:
+                logger.error('ReviewDocument not found for: %s %s' % (review, password,))
+                raise ObjectDoesNotExist
+
+            user = User.objects.get(pk=pk)
+
+        except Exception as e:
+            logger.error('ReviewDocument.auth does not exist: %s reason: %s' % (username, e))
+
+        return user
