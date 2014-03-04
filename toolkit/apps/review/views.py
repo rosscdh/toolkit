a48fffe171f539c150d0c33162c4765df3e38c11
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-04 15:49:04 +0100

    Added review app

--- toolkit/apps/review/views.py
+++ toolkit/apps/review/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.views.generic import DetailView
 
-# Create your views here.
+from dj_crocodoc.services import CrocoDocConnectService
+
+from .models import ReviewDocument
+
+
+class ReviewRevisionView(DetailView):
+    """
+    View to allow an authenticated user to view a crocodoc url that is connected
+    to a core.attachment revision
+    """
+    model = ReviewDocument
+    template_name = 'review/review.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs = super(ReviewRevisionView, self).get_context_data(**kwargs)
+        obj = kwargs.get('object', self.get_object())
+
+        crocodoc = CrocoDocConnectService(document_object=obj,
+                                         app_label='review',
+                                         field_name='attachment',
+                                         upload_immediately=True)
+
+        kwargs.update({
+            'crocodoc': crocodoc,
+        })
+
+        return kwargs
