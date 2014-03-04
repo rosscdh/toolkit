2d9ed7d3c0d8fb7a3144cfa4ee642d1bd4d68123
leejsinclair <lee.j.sinclair@gmail.com>
2014-03-05 00:09:04 +1000

    WIP: Added revoke capabilities, (not tested)

--- gui/service/participants.js
+++ gui/service/participants.js
@@ -7,7 +7,8 @@ angular.module('toolkit-gui').factory('participantService', [
 			return $resource('/api/v1/matters/:matterSlug/participant/:id', {}, {
 				'list': { 'method': 'GET', 'headers': { 'Content-Type': 'application/json'/*, 'token': token.value*/ }, 'isArray': true },
 				'get': { 'method': 'GET', 'headers': { 'Content-Type': 'application/json'/*, 'token': token.value*/ } },
-				'invite': { 'method': 'POST', 'headers': { 'Content-Type': 'application/json'/*, 'token': token.value*/ } }
+				'invite': { 'method': 'POST', 'headers': { 'Content-Type': 'application/json'/*, 'token': token.value*/ } },
+				'revoke': { 'method': 'DELETE', 'headers': { 'Content-Type': 'application/json'/*, 'token': token.value*/ } }
 			});
 		}
 		var participants = {
@@ -16,6 +17,21 @@ angular.module('toolkit-gui').factory('participantService', [
 				var api = participantAPI();
 
 				api.invite( { 'matterSlug': matterSlug }, details,
+					function success( response ) {
+						deferred.resolve( response );
+					},
+					function error( err ) {
+						deferred.reject( err );
+					}
+				);
+
+				return deferred.promise;
+			},
+			'revoke': function( matterSlug, details ) {
+				var deferred = $q.defer();
+				var api = participantAPI();
+
+				api.revoke( { 'matterSlug': matterSlug }, details,
 					function success( response ) {
 						deferred.resolve( response );
 					},
