6037e8ee6b5bcfe85ec4768c8a659d5280e83b7e
leejsinclair <lee.j.sinclair@gmail.com>
2014-03-04 21:44:00 +1000

    Participant dialog

--- gui/partial/participant-invite/participant-invite.js
+++ gui/partial/participant-invite/participant-invite.js
@@ -2,16 +2,40 @@ angular.module('toolkit-gui').controller('ParticipantInviteCtrl',[
 	'$scope',
 	'$modalInstance',
 	'participants',
+	'currentUser',
+	'matter',
 	'participantService',
-	function($scope, $modalInstance, participants){
+	function($scope, $modalInstance, participants, currentUser, matter, participantService){
 		$scope.participants = participants;
+		$scope.currentUser = currentUser;
+		$scope.matter = matter;
+
+		$scope.data = {
+			'invitee': { 'email': '', 'message': '' }
+		};
 
 		$scope.ok = function () {
 			$modalInstance.close( $scope.participants );
 		};
 
+		$scope.invite = function () {
+			participantService.invite( matter.selected.slug, $scope.data.invitee ).then(
+				function success() {
+					//
+				},
+				function error() {
+					//
+				}
+			);
+		};
+
 		$scope.cancel = function () {
 			$modalInstance.dismiss('cancel');
+		};
+
+		$scope.compareUrls = function( urla, urlb ) {
+			console.log(urla, urlb, urla.indexOf(urlb)>=0||urlb.indexOf(urla)>=0);
+			return urla.indexOf(urlb)>=0||urlb.indexOf(urla)>=0;
 		};
 	}
 ]);
