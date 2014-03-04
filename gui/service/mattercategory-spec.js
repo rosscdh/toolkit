12b3b3415cc4afb39abea7f5fdd3c3273a6cd07c
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2014-03-04 11:41:13 +0100

    [WIP] CRUD categories

---
+++ gui/service/mattercategory-spec.js
@@ -0,0 +1,11 @@
+describe('matterCategoryService', function() {
+
+  beforeEach(module('toolkit-gui'));
+
+  it('should ...', inject(function(matterCategoryService) {
+
+	//expect(matter.doSomething()).toEqual('something');
+
+  }));
+
+});
