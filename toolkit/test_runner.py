7636ecfe818c3932de87d1f197b94585fa4ccbc5
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-07 16:17:18 +0100

    renamed test files and added production tests

--- toolkit/test_runner.py
+++ toolkit/test_runner.py
@@ -2,8 +2,16 @@
 from django.conf import settings
 from django.core.management import call_command
 from django.test.simple import DjangoTestSuiteRunner
+
 from rainbowrunners.djrunner import NyanCatDiscoverRunner
+
+import os
 import shutil
+
+PROD_SETTINGS_SRC = os.path.join(settings.SITE_ROOT, 'conf', 'production.local_settings.py')
+PROD_SETTINGS_DEST = os.path.join(settings.SITE_ROOT, 'toolkit', 'production_settings.py')
+
+assert PROD_SETTINGS_SRC, 'Must have a conf/production.local_settings.py defined'
 
 
 class AppTestRunner(NyanCatDiscoverRunner, DjangoTestSuiteRunner):
@@ -19,10 +27,21 @@ class AppTestRunner(NyanCatDiscoverRunner, DjangoTestSuiteRunner):
 
         return super(AppTestRunner, self).build_suite(test_labels, *args, **kwargs)
 
-    # def setup_test_environment(self, **kwargs):
-    #     call_command('collectstatic', interactive=False)  # collect static so our casper tests break less
-    #     super(AppTestRunner, self).setup_test_environment(**kwargs)
+    def setup_test_environment(self, **kwargs):
+        # call_command('collectstatic', interactive=False)  # collect static so our casper tests break less
 
-    # def teardown_test_environment(self, **kwargs):
-    #     shutil.rmtree(settings.STATIC_ROOT)  # delete the static folder
-    #     super(AppTestRunner, self).teardown_test_environment(**kwargs)
+        # copy the conf/production.local_settings.py to toolkit/production_settings.py
+        # so that we can test the various production settings *in mattters for eg
+        shutil.copyfile(PROD_SETTINGS_SRC, PROD_SETTINGS_DEST)
+
+        assert os.path.isfile(PROD_SETTINGS_DEST), 'Could not copy the conf/production.local_settings.py file'
+
+        super(AppTestRunner, self).setup_test_environment(**kwargs)
+
+    def teardown_test_environment(self, **kwargs):
+        #shutil.rmtree(settings.STATIC_ROOT)  # delete the static folder
+
+        # Remove the prod settings which are only present so we can test them
+        os.remove(PROD_SETTINGS_DEST)
+
+        super(AppTestRunner, self).teardown_test_environment(**kwargs)
