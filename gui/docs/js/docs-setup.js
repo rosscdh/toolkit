e80e902aa1fff121d1ee34d2236b790eadc63196
Lee <lee@kudosknowledge.com>
2014-03-09 19:07:56 +1000

    Added documentation

---
+++ gui/docs/js/docs-setup.js
@@ -0,0 +1,14 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
