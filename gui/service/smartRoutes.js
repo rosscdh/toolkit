315b6eeab088ef9d43e468628ba0a0bb9f031d8e
leejsinclair <lee.j.sinclair@gmail.com>
2014-03-10 22:57:20 +1000

    Created smart routes service

---
+++ gui/service/smartRoutes.js
@@ -0,0 +1,37 @@
+angular.module('toolkit-gui').factory('smartRoutes', [ '$routeParams', function($routeParams) {
+
+	var paths = [
+		{ 'pattern': '/matters/:matterSlug', 'match': /^\/matters/i }
+	];
+
+	function parseUrl( url ) {
+		var a = document.createElement('a');
+		a.href = url;
+		return a;
+	}
+
+	var smartRoutes = {
+		'params': function() {
+			var urlPath = parseUrl(window.location).pathname;
+			var pathEls = [];
+			var urlPathEls = [];
+			var matchingPath = jQuery.grep( paths, function( path ) {
+				return urlPath.match( path.match ); 
+			});
+
+			if( matchingPath && matchingPath.length>0 ) {
+				pathEls = matchingPath[0].pattern.split('/');
+				urlPathEls = urlPath.split('/');
+
+				for(var i=0;i<pathEls.length;i++) {
+					if( pathEls[i].indexOf(':')===0 ) {
+						$routeParams[ pathEls[i].replace(':','') ] = urlPathEls[i];
+					}
+				}
+			}
+			return $routeParams;
+		}
+	};
+
+	return smartRoutes;
+}]);
