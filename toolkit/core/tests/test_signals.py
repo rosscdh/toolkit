3ebaaabba97f8506e2377fab2ee5cd051e72b8a8
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-10 08:44:47 +0100

    cleaning up tests

--- toolkit/core/tests/test_signals.py
+++ toolkit/core/tests/test_signals.py
@@ -1,15 +1,19 @@
-import json
-from actstream.models import Action
-from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
-from django.test import TestCase
+# -*- coding: utf-8 -*-
 from django.core.cache import cache
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+from django.test import TestCase
 from django.dispatch import receiver
+
 from model_mommy import mommy
-from toolkit.api.serializers import ItemSerializer
+from actstream.models import Action
+from actstream.models import action_object_stream
+
 from toolkit.casper import BaseScenarios
+from toolkit.api.serializers import ItemSerializer
 from toolkit.core.signals import send_activity_log
 
+import json
 
 cache_key = 'activity_stream_signal_received_keys'
 
@@ -19,65 +23,35 @@ def on_activity_received(**kwargs):
     """
     Test signal listener to handle the signal fired event
     """
-    cache.set(cache_key, kwargs.keys())
+    for i in kwargs:
+        if type(kwargs[i]) not in [str, unicode,]:
+            kwargs[i] = str(type(kwargs[i]))
+
+    cache.set(cache_key, kwargs)
 
 
-class ActivitySignalTestCase(BaseScenarios, TestCase):
+class ActivitySignalTest(BaseScenarios, TestCase):
 
     def setUp(self):
-        super(ActivitySignalTestCase, self).setUp()
+        super(ActivitySignalTest, self).setUp()
         self.basic_workspace()
-        self.test_lawyer_post_item = mommy.prepare('item.Item', matter=self.workspace, name='New Test Item for test_lawyer_post')
 
-    def test_workspace_created_signal_received(self):
+    def test_workspace_created_signal_received_on_basic_workspace(self):
+        """
+        Created by the self.basic_workspace() call
+        """
         # in setUp the workspace was created which should have reached on_activity_received above:
-        self.assertItemsEqual(cache.get(cache_key), ['sender', 'signal', 'actor', 'verb', 'action_object', 'target', 'ip'])
+        cache_obj = cache.get(cache_key)
+        self.assertItemsEqual(cache_obj.keys(), ['sender', 'signal', 'actor', 'verb', 'action_object', 'target', 'ip'])
+        self.assertItemsEqual(cache_obj.values(), ["<class 'django.db.models.base.ModelBase'>", '127.0.0.1', "<class 'django.dispatch.dispatcher.Signal'>", "<class 'django.contrib.auth.models.User'>", u'created', "<class 'toolkit.apps.workspace.models.Workspace'>", "<class 'toolkit.apps.workspace.models.Workspace'>"])
         cache.delete(cache_key)
 
-    def test_workspace_created_action_created(self):
-        action = Action.objects.get(
-            actor_content_type=ContentType.objects.get_for_model(self.lawyer),
-            actor_object_id=self.lawyer.id,
-            verb=u'created',
-            action_object_content_type=ContentType.objects.get_for_model(self.workspace),
-            action_object_object_id=self.workspace.id,
-            target_content_type=ContentType.objects.get_for_model(self.workspace),
-            target_object_id=self.workspace.id
-        )
-        self.assertEqual(action.data['ip'], '127.0.0.1')
+    def test_stream_item_created_manually(self):
+        workspace = mommy.make('workspace.Workspace', name='Action Created by Signal Workspace', lawyer=self.lawyer)
+        stream = action_object_stream(workspace)
+        self.assertEqual(len(stream), 1)
 
-    """
-    TODO: as soon as signal is used (meaning: it got bound to certain events (object got modified, object got commented, ...))
-            we should add tests for these usages.
-
-            possibly similar to the following.
-    """
-
-    @property
-    def endpoint(self):
-        return reverse('matter_items', kwargs={'matter_slug': self.workspace.slug})
-
-    # adapted from api-endpoints-test
-    def test_lawyer_post(self):
-        self.client.login(username=self.lawyer.username, password=self.password)
-        self.client.post(self.endpoint, json.dumps(ItemSerializer(self.test_lawyer_post_item).data), content_type='application/json')
-
-    def test_item_created(self):
-        # do API-request to create new item and afterwards check if the corresponding action got created
-        self.test_lawyer_post()
-
-        # missing part in API or item-model: send signal
-
-        try:
-            action = Action.objects.get(
-                actor_content_type=ContentType.objects.get_for_model(self.lawyer),
-                actor_object_id=self.lawyer.id,
-                verb=u'created',
-                action_object_content_type=ContentType.objects.get_for_model(self.test_lawyer_post_item),
-                action_object_object_id=self.test_lawyer_post_item.id,
-                target_content_type=ContentType.objects.get_for_model(self.workspace),
-                target_object_id=self.workspace.id
-            )
-        except Action.DoesNotExist:
-            pass
-        # self.assertEqual(action.data['ip'], '127.0.0.1')
+        stream_item = stream[0]
+        self.assertEqual(stream_item.verb, 'created')
+        self.assertEqual(stream_item.target, workspace)
+        self.assertEqual(stream_item.actor, self.lawyer)
