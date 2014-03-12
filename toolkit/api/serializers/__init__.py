8d95a94e54a6b45a6afc1d9a93973c034aca8544
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-12 11:23:58 +0100

    Added item serializer and item activate endpoint to show activity based on matter level as well as item level

--- toolkit/api/serializers/__init__.py
+++ toolkit/api/serializers/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 from .hateoas import HATOAS
 
-from .account import AccountSerializer, PasswordSerializer
-from .activity import ActivitySerializer
-from .client import ClientSerializer, LiteClientSerializer
-from .matter import MatterSerializer, LiteMatterSerializer
-from .item import ItemSerializer
-from .revision import RevisionSerializer
-from .user import UserSerializer, LiteUserSerializer, SimpleUserSerializer
+from .account import (AccountSerializer, PasswordSerializer)
+from .activity import (MatterActivitySerializer, ItemActivitySerializer,)
+from .client import (ClientSerializer, LiteClientSerializer)
+from .matter import (MatterSerializer, LiteMatterSerializer)
+from .item import (ItemSerializer,)
+from .revision import (RevisionSerializer,)
+from .user import (UserSerializer, LiteUserSerializer, SimpleUserSerializer,)
 # from .workflow import WorkflowSerializer
