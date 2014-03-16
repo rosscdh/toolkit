335739db6b3576d04643d1106b257054345969b6
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-16 11:56:14 +0100

    removed users

--- gui/partial/checklist/checklist.js
+++ gui/partial/checklist/checklist.js
@@ -47,11 +47,7 @@ angular.module('toolkit-gui')
 			'selectedItem': null,
 			'selectedCategory': null,
 			'categories': [],
-			'users': [
-				{ 'name': 'Sam Jackson', 'img': 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash3/t1/c0.0.100.100/p100x100/1014416_10100118438650161_136799916_a.jpg' },
-				{ 'name': 'Bob Jackson', 'img': 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash2/t1/c12.12.155.155/314032_10150303812474864_594285312_a.jpg' },
-				{ 'name': 'Hugh Jackson', 'img': 'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/t1/c42.26.328.328/s320x320/229934_10150955684263644_890325486_n.jpg' }
-			],
+			'users': [],
 			'searchData': searchService.data()
 		};
 
