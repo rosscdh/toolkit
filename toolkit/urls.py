da1713abca8ee0af5f900d11b8f455684d422595
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-16 11:56:40 +0100

    made TEST_PREPROD test more explicit

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -40,7 +40,7 @@ urlpatterns = patterns('',
     url(r'^', include('toolkit.apps.default.urls', namespace='public')),
 )
 
-if settings.DEBUG is True or settings.TEST_PREPROD:
+if settings.DEBUG is True or settings.TEST_PREPROD is True:
     # Add the MEDIA_URL to the dev environment
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
