1fcf33571dcd133acd90375610de926e1a1f2ba8
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-16 10:08:39 +0100

    removing unused and importing our custom static handler

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.conf.urls.static import static
+from toolkit.static import static
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -44,3 +42,4 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     # Add the MEDIA_URL to the dev environment
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
