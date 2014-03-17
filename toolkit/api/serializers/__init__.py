754ded2572bca883ff3ccab5c230a2eb311b603f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-17 14:50:32 +0100

    added userserializer with user_review_url

--- toolkit/api/serializers/__init__.py
+++ toolkit/api/serializers/__init__.py
@@ -7,5 +7,8 @@ from .client import (ClientSerializer, LiteClientSerializer)
 from .matter import (MatterSerializer, LiteMatterSerializer, SimpleMatterSerializer)
 from .item import (ItemSerializer,)
 from .revision import (RevisionSerializer,)
-from .user import (UserSerializer, LiteUserSerializer, SimpleUserSerializer,)
+from .user import (UserSerializer,
+                   LiteUserSerializer,
+                   SimpleUserSerializer,
+                   SimpleUserWithReviewUrlSerializer)
 # from .workflow import WorkflowSerializer
