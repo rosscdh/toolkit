aa60faa509586ea8862df0a79ee380ecfcad0968
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-17 18:15:33 +0100

    Added signal to handle setting item.is_requested = False when a new revision is added

--- toolkit/core/attachment/models.py
+++ toolkit/core/attachment/models.py
@@ -102,5 +102,8 @@ class Revision(models.Model):
 
 post_save.connect(on_revision_post_save, sender=Revision)
 
-
-from .signals import (ensure_revision_slug,)
+from .signals import (ensure_revision_slug,
+                      set_item_is_requested_false,
+                      ensure_revision_reviewdocument_object,
+                      on_reviewer_add,
+                      on_reviewer_remove)
