b6309ff9f55c307084143e8940d6c9ad5c9b1cc9
Jamie Connolly <jamie@jamieconnolly.com>
2014-03-18 18:59:16 +0100

    Add in the new queryset for all the different request types

--- toolkit/core/item/managers.py
+++ toolkit/core/item/managers.py
@@ -1,4 +1,8 @@
 # -*- coding: UTF-8 -*-
+import operator
+
+from django.db import models
+
 from toolkit.core.mixins import IsDeletedManager
 
 from .query import ItemQuerySet
@@ -13,3 +17,15 @@ class ItemManager(IsDeletedManager):
 
     def get_queryset(self):
         return ItemQuerySet(self.model, using=self._db).filter(is_deleted=False)
+
+    def my_requests(self, user):
+        queries = []
+
+        # upload requests
+        queries += [models.Q(responsible_party=user) & models.Q(is_requested=True)]
+        # review requests
+        queries += [models.Q(revision__is_current=True) & models.Q(revision__reviewers__in=[user])]
+        # signing requests
+        queries += [models.Q(revision__is_current=True) & models.Q(revision__signatories__in=[user])]
+
+        return self.get_queryset().filter(reduce(operator.or_, queries))
