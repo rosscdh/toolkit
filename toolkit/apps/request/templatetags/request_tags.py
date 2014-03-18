0ac9d8a0dfc0ef2852284a432e08495f50e75818
Jamie Connolly <jamie@jamieconnolly.com>
2014-03-18 18:48:41 +0100

    Split out the request list block into the 3 subfiles

--- toolkit/apps/request/templatetags/request_tags.py
+++ toolkit/apps/request/templatetags/request_tags.py
@@ -5,43 +5,22 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('request/request_list_block.html', takes_context=False)
+@register.inclusion_tag('request/request_list_upload.html', takes_context=False)
 def requests_upload_section(object_list, user):
     return {
-        'action_btn': u'Upload File',
-        'description': u'Please upload the following documents:',
-        'icon': 'fui-upload',
-        'object_list': object_list.needs_upload(user),
-        'story': u'requested you upload this document.',
-        'title': u'Documents to be uploaded',
-        'toggle': u'filepicker',
-        'type': u'upload',
+        'object_list': object_list.needs_upload(user)
     }
 
 
-@register.inclusion_tag('request/request_list_block.html', takes_context=False)
+@register.inclusion_tag('request/request_list_review.html', takes_context=False)
 def requests_review_section(object_list, user):
     return {
-        'action_btn': u'Review',
-        'description': u'Please read and review the following documents:',
-        'icon': 'fui-upload',
         'object_list': object_list.needs_review(user),
-        'story': u'requested your feedback on this document',
-        'title': u'Documents requiring review',
-        'toggle': None,
-        'type': u'review',
     }
 
 
-@register.inclusion_tag('request/request_list_block.html', takes_context=False)
+@register.inclusion_tag('request/request_list_signing.html', takes_context=False)
 def requests_signing_section(object_list, user):
     return {
-        'action_btn': u'Review and Sign',
-        'description': u'Please read, review and sign the following documents:',
-        'icon': 'fui-upload',
         'object_list': object_list.needs_signature(user),
-        'story': u'requested your signature on this document.',
-        'title': u'Documents to be signed',
-        'toggle': None,
-        'type': u'signing',
     }
