6ebf058c88cadad68f06d71ab278b12dbfd0b4f0
Ross Crawford <sendrossemail@gmail.com>
2014-03-18 22:24:42 +0100

    finally working export settings

--- conf/dev.local_settings.py
+++ conf/dev.local_settings.py
@@ -14,7 +14,7 @@ if TEST_PREPROD is True:
     STATICFILES_DIRS = (
         # These are the production files
         # not that static is in gui/dist/static *not to be confused with the django {{ STATIC_URL }}ng/ which will now point correctly
-        ("ng", os.path.join(SITE_ROOT, 'gui', 'dist', 'static', 'ng')),
+        ("ng", os.path.join(SITE_ROOT, 'gui', 'dist')),
     )
 
 INSTALLED_APPS = INSTALLED_APPS + (
