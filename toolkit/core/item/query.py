c54efeaf85aeb646bac67a09abed200c4b22ddde
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-20 12:22:51 +0100

    removing Q objects on filter

--- toolkit/core/item/query.py
+++ toolkit/core/item/query.py
@@ -8,13 +8,13 @@ class ItemQuerySet(QuerySet):
         return self.filter(responsible_party=user, **kwargs)
 
     def needs_review(self, user):
-        return self.filter(models.Q(revision__is_current=True) & models.Q(revision__reviewers__in=[user]))
+        return self.filter(revision__is_current=True, revision__reviewers__in=[user])
 
     def needs_signature(self, user):
-        return self.filter(models.Q(revision__is_current=True) & models.Q(revision__signatories__in=[user]))
+        return self.filter(revision__is_current=True, revision__signatories__in=[user])
 
     def needs_upload(self, user):
-        return self.filter(models.Q(responsible_party=user) & models.Q(is_requested=True))
+        return self.filter(responsible_party=user, is_requested=True)
 
     def requested(self, **kwargs):
         return self.filter(is_requested=True, **kwargs)
