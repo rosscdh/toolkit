664497d577cc6e5c1588ccac6f17aca8fa122626
marbur <marius.burfey@ambient-innovation.com>
2014-03-20 15:38:08 +0100

    started with deletion

--- toolkit/api/views/comment.py
+++ toolkit/api/views/comment.py
@@ -1,15 +1,18 @@
 # -*- coding: UTF-8 -*-
-from actstream.models import Action
+from actstream.models import Action, model_stream
 from rest_framework import generics
 from rest_framework import status as http_status
 from rest_framework.response import Response
 from toolkit.api.serializers import ItemActivitySerializer
 from toolkit.api.views.mixins import MatterItemsQuerySetMixin
 from rulez import registry as rulez_registry
+from toolkit.core.item.models import Item
 from toolkit.core.services.matter_activity import MatterActivityEventService
 
 
-class ItemCommentEndpoint(MatterItemsQuerySetMixin, generics.CreateAPIView):
+class ItemCommentEndpoint(MatterItemsQuerySetMixin,
+                          generics.CreateAPIView,
+                          generics.DestroyAPIView):
     """
     Endpoint to post new comments for item in matter
     """
@@ -30,5 +33,11 @@ class ItemCommentEndpoint(MatterItemsQuerySetMixin, generics.CreateAPIView):
     def can_edit(self, user):
         return user.profile.user_class in ['lawyer', 'customer']
 
+    def can_delete(self, user):
+        bla = model_stream(Item)[:1][0].actor == user or user.profile.is_lawyer
+        import pdb
+        pdb.set_trace()
+        return bla
 
 rulez_registry.register("can_edit", ItemCommentEndpoint)
+rulez_registry.register("can_delete", ItemCommentEndpoint)
