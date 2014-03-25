abda9cba78b77f5322ea65748804c4f5247cc8bf
Lee <lee@kudosknowledge.com>
2014-03-25 23:20:17 +1000

    Cleaning up comments

--- gui/service/activity-spec.js
+++ gui/service/activity-spec.js
@@ -17,22 +17,18 @@ describe('activityService', function() {
   });
 
   it('should have method:matterstream', inject([ function() {
-	//expect(matter.doSomething()).toEqual('something');
 	expect(typeof activityService.matterstream).toBe('function');
   }]));
 
   it('should have method:itemstream', inject([ function() {
-	//expect(matter.doSomething()).toEqual('something');
 	expect(typeof activityService.itemstream).toBe('function');
   }]));
 
   it('should have an API_BASE', inject([ 'API_BASE_URL', function( API_BASE_URL ) {
-	//expect(matter.doSomething()).toEqual('something');
 	expect(API_BASE_URL).toEqual('/api/v1/');
   }]));
 
   it('should make API call: matters/:slug/activity', inject([ 'API_BASE_URL', function( API_BASE_URL ) {
-	//expect(matter.doSomething()).toEqual('something');
 	$httpBackend.expect( 'GET', API_BASE_URL + 'matters/matter-slug/activity' ).respond( { 'results': [{'title':'test title'}] } );
 
 	var resultPromise,
@@ -42,8 +38,6 @@ describe('activityService', function() {
     activityService.matterstream('matter-slug').then(function (result) {
       resultPromise=result;
     });
-	// matterstream
-	//var result = activityService.matterstream('test');
 
 	$httpBackend.flush();
 
@@ -52,7 +46,6 @@ describe('activityService', function() {
   }]));
 
   it('should make API call: matters/:matterSlug/items/:itemSlug/activity', inject([ 'API_BASE_URL', function( API_BASE_URL ) {
-	//expect(matter.doSomething()).toEqual('something');
 	$httpBackend.expect( 'GET', API_BASE_URL + 'matters/matter-slug/items/item-slug/activity' ).respond( { 'results': [{'title':'test title'}] } );
 
 	var resultPromise,
@@ -62,8 +55,6 @@ describe('activityService', function() {
     activityService.itemstream('matter-slug', 'item-slug').then(function (result) {
       resultPromise=result;
     });
-	// matterstream
-	//var result = activityService.matterstream('test');
 
 	$httpBackend.flush();
 
