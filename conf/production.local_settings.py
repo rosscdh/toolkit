915d6ac1fd65acd4f7eaea88ff87e8323eee14ca
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-27 13:10:08 +0100

    getting production loggers sorted

--- conf/production.local_settings.py
+++ conf/production.local_settings.py
@@ -97,3 +97,67 @@ ABRIDGE_ACCESS_KEY_ID = 'e4b38a5758caf486e21c'
 ABRIDGE_SECRET_ACCESS_KEY = '2a2c7c6104c80855a12d53bd846e117fbf81f41c'
 ABRIDGE_USERNAME = 'lawpal-production'
 ABRIDGE_PASSWORD = 'production123'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'medium': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'medium'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'formatter': 'medium'
+        },
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/tmp/toolkit-{env}.log'.format(env='dev')
+        },
+        'splunkstorm':{
+            'level': 'INFO',
+            'class': 'toolkit.loggers.SplunkStormLogger',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['splunkstorm'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'splunkstorm', 'logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['splunkstorm'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.test': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    }
+}
