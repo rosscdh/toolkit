48119ca2a4d0da2652039e9b99795a4bf4530c66
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-03 14:52:28 -0700

    Added migration

--- toolkit/apps/default/mixins.py
+++ toolkit/apps/default/mixins.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
+
 class EmailIsValidatedMixin(object):
+    """
+    Mixin to provide access to saving the validated_email
+    property for user.profile
+    """
     @property
     def validated_email(self):
         return self.data.get('validated_email', False)
