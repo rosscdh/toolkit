49417b4ebf58ecee2027d0dde0ce3b76a358b747
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-03 18:10:28 -0700

    better admin for workspace attach item

--- toolkit/apps/workspace/admin.py
+++ toolkit/apps/workspace/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Workspace, Tool
 
+class WorkspaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'slug', 'matter_code',)
+    search_fields = ('name', 'slug',)
 
-admin.site.register([Workspace, Tool])
+admin.site.register(Workspace, WorkspaceAdmin)
+admin.site.register([Tool])
