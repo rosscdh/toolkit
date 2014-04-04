31cdded72d4047d5fccb1c201439955309b51f86
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-03 18:10:28 -0700

    better admin for workspace attach item

--- toolkit/core/attachment/admin.py
+++ toolkit/core/attachment/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 
 from .models import Revision
 
-admin.site.register([Revision])
+
+class RevisionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'slug', 'status',)
+    search_fields = ('name', 'slug',)
+    list_filter = ['status']
+
+
+admin.site.register(Revision, RevisionAdmin)
