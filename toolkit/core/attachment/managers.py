ac9fb146dcfad66a717e75053be68036977e7ad8
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-05 09:09:03 -0700

    added prefetch

--- toolkit/core/attachment/managers.py
+++ toolkit/core/attachment/managers.py
@@ -3,5 +3,9 @@ from django.db import models
 
 
 class RevisionManager(models.Manager):
+    def get_query_set(self):
+        qs = super(RevisionManager, self).get_query_set()
+        return qs.prefetch_related('revision_reviewers', 'revision_signers').select_related('uploaded_by')
+
     def current(self):
         return self.filter(is_current=True)
