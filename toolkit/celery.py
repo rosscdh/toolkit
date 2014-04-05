6b7172e9c51df7ecd4cfc3598c3e7fa6723b5f6d
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-04 20:52:50 -0700

    little fixes

--- toolkit/celery.py
+++ toolkit/celery.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from django.conf import settings
 
 from celery import Celery
-from django.conf import settings
+from kombu import Exchange, Queue
 
 import os
 import urllib
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolkit.settings')
@@ -13,6 +15,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toolkit.settings')
 BROKER_URL = 'sqs://{BROKER_USER}:{BROKER_PASSWORD}@sqs.us-west-1.amazonaws.com/562971026743/{CELERY_DEFAULT_QUEUE}'.format(BROKER_USER=urllib.quote(settings.AWS_ACCESS_KEY_ID, safe=''),
                                                                                                                             BROKER_PASSWORD=urllib.quote(settings.AWS_SECRET_ACCESS_KEY, safe=''),
                                                                                                                             CELERY_DEFAULT_QUEUE=settings.CELERY_DEFAULT_QUEUE)
+CELERY_QUEUES = (
+    Queue('default', Exchange('default'), routing_key='default'),
+)
 
 app = Celery('toolkit',
              broker=BROKER_URL,)
