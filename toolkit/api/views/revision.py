4d64674b3d631a6f8a4a8b21d5c51afe4e5c644a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-04 23:23:52 -0700

    commented out async task crocodoc

--- toolkit/api/views/revision.py
+++ toolkit/api/views/revision.py
@@ -11,7 +11,7 @@ from rest_framework.response import Response
 
 from toolkit.core.attachment.models import Revision
 
-from toolkit.apps.review.tasks import crocodoc_upload_task
+#from toolkit.apps.review.tasks import crocodoc_upload_task
 
 from .mixins import (MatterItemsQuerySetMixin,)
 
@@ -134,7 +134,7 @@ class ItemCurrentRevisionView(generics.CreateAPIView,
             #     crocodoc_upload_task.delay(user=self.request.user, revision=self.object)
             # except Exception as e:
             #     logger.critical('Could not call crocodoc upload process asyncronously: %s' % e)
-            crocodoc_upload_task(user=self.request.user, revision=self.object)
+            # crocodoc_upload_task(user=self.request.user, revision=self.object)
 
             #
             # Custom signal event
