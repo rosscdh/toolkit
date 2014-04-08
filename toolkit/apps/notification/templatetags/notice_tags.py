b5585639b7b53dd02a5b3e3d132f4ac8c675be79
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-08 12:24:18 -0700

    Fixing notice_tags breaking for alex and his ancient notifications

--- toolkit/apps/notification/templatetags/notice_tags.py
+++ toolkit/apps/notification/templatetags/notice_tags.py
@@ -10,15 +10,19 @@ logger = logging.getLogger('django.request')
 @register.inclusion_tag('notification/partials/notice.html', takes_context=False)
 def render_notice(notice):
     message = notice.message.data
+    actor = message.get('actor')
+    target = message.get('target')
+    client = message.get('client')
+
     if message is not None:
         return {
             'pk': notice.pk,
-            'actor_name': message.get('actor').get('name'),
-            'actor_initials': message.get('actor').get('initials'),
+            'actor_name': actor.get('name') if actor else None,
+            'actor_initials': actor.get('initials') if actor else None,
             'message': notice.message,
             'date': notice.message.date,
-            'base_url': message.get('target').get('base_url'),
-            'target_name': message.get('target').get('name'),
-            'client_name': message.get('target').get('client').get('name'),
+            'base_url': target.get('base_url') if target else None,
+            'target_name': target.get('name') if target else None,
+            'client_name': target.get('client').get('name') if client else None,
             'notice_message': notice.message
         }
