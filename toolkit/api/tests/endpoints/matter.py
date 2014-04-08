9300af5168f2dce11f69b5ce3a682b3f4e7893c6
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-08 10:08:50 -0700

    getting tests passing

--- toolkit/api/tests/endpoints/matter.py
+++ toolkit/api/tests/endpoints/matter.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import reverse
 from toolkit.core.item.models import Item
 from toolkit.core.attachment.models import Revision
 from toolkit.apps.workspace.models import Workspace
+from toolkit.apps.default.templatetags.toolkit_tags import ABSOLUTE_BASE_URL
 
 from . import BaseEndpointTest
 from ...serializers import LiteClientSerializer
@@ -322,8 +323,10 @@ class MatterDetailProvidedDataTest(BaseEndpointTest):
         self.assertEqual(type(items), list)
 
         latest_revision = items[0].get('latest_revision')
-        self.assertEqual(type(latest_revision), dict)
-        self.assertEqual(latest_revision.get('url'), 'http://testserver/api/v1/revisions/%d' % self.revision.pk)
+        self.assertEqual(type(latest_revision), unicode)
+
+        expected_url = ABSOLUTE_BASE_URL(reverse('matter_item_revision', kwargs={'matter_slug': self.revision.item.matter.slug, 'item_slug': self.revision.item.slug }))
+        self.assertEqual(latest_revision, expected_url)
 
     def test_endpoint_data_lawyer(self):
         self.client.login(username=self.lawyer.username, password=self.password)
