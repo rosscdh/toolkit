289e0b3ebf22d4538ed31fd65dfdd0a70b18c7a3
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-08 19:29:28 -0700

    Better client admin and changed workspace verbose name

--- toolkit/core/client/admin.py
+++ toolkit/core/client/admin.py
@@ -3,4 +3,15 @@ from django.contrib import admin
 
 from .models import Client
 
-admin.site.register([Client])
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'lawyer')
+    search_fields = ('name', 'slug', 'lawyer__first_name', 'lawyer__last_name')
+
+    def queryset(self, request):
+        return super(ClientAdmin, self).queryset(request=request).select_related('lawyer')
+
+
+admin.site.register(Client, ClientAdmin)
+
+
