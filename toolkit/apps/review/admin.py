d9f357e2f4f0d79f259dc1e85220b5b0a320702c
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-08 20:16:00 -0700

    getting admin for reviews started

--- toolkit/apps/review/admin.py
+++ toolkit/apps/review/admin.py
@@ -4,4 +4,12 @@ from django.contrib import admin
 from .models import ReviewDocument
 
 
-admin.site.register([ReviewDocument])
+class ReviewDocumentAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'document',)
+    #search_fields = ('name', 'slug', 'lawyer__first_name', 'lawyer__last_name')
+
+    def queryset(self, request):
+        return super(ReviewDocumentAdmin, self).queryset(request=request).select_related('document')
+
+
+admin.site.register(ReviewDocument, ReviewDocumentAdmin)
