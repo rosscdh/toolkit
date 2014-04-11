eda886708fc56a9086ed91c352bf51c99066740b
marbur <marius.burfey@ambient-innovation.com>
2014-04-11 14:50:51 +0200

    created templates for activity-stream-objects

--- toolkit/api/serializers/activity.py
+++ toolkit/api/serializers/activity.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from django.core.serializers.base import Serializer
-from django.utils.translation import ugettext as _
 from django.template import loader
 
 from actstream.models import Action
@@ -9,10 +7,12 @@ from rest_framework import serializers
 from toolkit.api.serializers.item import LiteItemSerializer
 from toolkit.api.serializers.user import LiteUserSerializer
 
+default_template = loader.get_template('activity/default.html')  # allow override of template_name
+item_comment_template = loader.get_template('activity/item_comment.html')  # allow override of template_name
+revision_comment_template = loader.get_template('activity/revision_comment.html')  # allow override of template_name
 
-def _get_comment_display(ctx, comment):
-    template = loader.get_template('activity/comment.html')  # allow override of template_name
-    ctx['comment'] = comment
+
+def _get_activity_display(ctx, template):
     context = loader.Context(ctx)
     # render the template with passed in context
     return template.render(context)
@@ -25,7 +25,7 @@ class MatterActivitySerializer(serializers.HyperlinkedModelSerializer):
     actor = LiteUserSerializer('actor')
     action_object = LiteItemSerializer('action_object')
                         # TODO: change this to a generic one with URL. action_object can be Item/Revision/Matter/...
-                        # instead we could/should use the rendered event-html in the gui
+                        # instead we could use the rendered event-html in the gui
 
     class Meta:
         model = Action
@@ -50,50 +50,65 @@ class MatterActivitySerializer(serializers.HyperlinkedModelSerializer):
             'action_object_url': obj.action_object.get_absolute_url() if obj.action_object and hasattr(obj.action_object, 'get_absolute_url') else None,
             'timestamp': obj.timestamp,
             'timesince': obj.timesince(),
+            'data': obj.data,
             #'target': obj.target,
             #'target_pk': obj.target.slug,
         }
 
-        override_message = obj.data.get('override_message', None)
-        comment = obj.data.get('comment', None)
+        # override_message = obj.data.get('override_message', None)
 
-        if comment is not None:
-            return _get_comment_display(ctx, comment)
+        # TODO: move get_verb_slug out of MatterActivityEventService
+        verb_slug = obj.target.actions.get_verb_slug(obj.action_object, obj.verb)
+        template = default_template
 
-        if override_message is not None:
-            return override_message % ctx
+        if verb_slug == 'item-commented':
+            # comment-template
+            template = item_comment_template
 
-        if obj.action_object.__class__.__name__ in ['Item']:
-            return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
+        if verb_slug == 'revision-added-revision-comment':
+            # crocodoc-template
+            template = revision_comment_template
 
-        return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
+        return _get_activity_display(ctx, template)
+        #
+        # if comment is not None:
+        #     return _get_comment_display(ctx, comment)
+        #
+        # if override_message is not None:
+        #     return override_message % ctx
+        #
+        # if obj.action_object.__class__.__name__ in ['Item']:
+        #     return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
+        #
+        # return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
 
 
 class ItemActivitySerializer(MatterActivitySerializer):
-    def get_event(self, obj):
-        """
-        Item Actions should show a bit more info about the event
-        """
-        ctx = {
-            'actor': obj.actor,
-            'actor_pk': obj.actor.pk,
-            'verb': obj.verb,
-            'action_object': obj.action_object,
-            'action_object_pk': obj.action_object.slug if obj.action_object else None,
-            'action_object_url': obj.action_object.get_absolute_url() if obj.action_object and hasattr(obj.action_object, 'get_absolute_url') else None,
-            'timestamp': obj.timestamp,
-            'timesince': obj.timesince(),
-            #'target': obj.target,
-            #'target_pk': obj.target.slug,
-        }
-        comment = obj.data.get('comment', None)
-
-        if comment is not None:
-            return _get_comment_display(ctx, comment)
-
-        override_message = obj.data.get('override_message', None)
-
-        if override_message is not None:
-            return override_message % ctx
-
-        return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
+    pass
+    # def get_event(self, obj):
+    #     """
+    #     Item Actions should show a bit more info about the event
+    #     """
+    #     ctx = {
+    #         'actor': obj.actor,
+    #         'actor_pk': obj.actor.pk,
+    #         'verb': obj.verb,
+    #         'action_object': obj.action_object,
+    #         'action_object_pk': obj.action_object.slug if obj.action_object else None,
+    #         'action_object_url': obj.action_object.get_absolute_url() if obj.action_object and hasattr(obj.action_object, 'get_absolute_url') else None,
+    #         'timestamp': obj.timestamp,
+    #         'timesince': obj.timesince(),
+    #         #'target': obj.target,
+    #         #'target_pk': obj.target.slug,
+    #     }
+    #     comment = obj.data.get('comment', None)
+    #
+    #     if comment is not None:
+    #         return _get_comment_display(ctx, comment)
+    #
+    #     override_message = obj.data.get('override_message', None)
+    #
+    #     if override_message is not None:
+    #         return override_message % ctx
+    #
+    #     return _('<span data-uid="%(actor_pk)d">%(actor)s</span> %(verb)s <a href="%(action_object_url)s">%(action_object)s</a> <span data-date="%(timestamp)s"></span>') % ctx
