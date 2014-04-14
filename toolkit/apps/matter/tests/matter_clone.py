5150a4319751597981315d930415a1a227dc6687
Ross Crawford <sendrossemail@gmail.com>
2014-04-14 23:09:24 +0200

    expanded on clone tests but are still green

--- toolkit/apps/matter/tests/matter_clone.py
+++ toolkit/apps/matter/tests/matter_clone.py
@@ -17,12 +17,17 @@ class MatterCloneTest(TestCase):
         # items to clone
         self.item = mommy.make('item.Item', matter=self.source, name='Test Item from Source No.1', category=None)
         mommy.make('attachment.Revision', executed_file=None, slug=None, item=self.item, uploaded_by=self.lawyer)
+        
 
         self.item2 = mommy.make('item.Item', matter=self.source, name='Test Item from Source No.2', category=None)
         mommy.make('attachment.Revision', executed_file=None, slug=None, item=self.item2, uploaded_by=self.lawyer)
 
+
     def test_clone_service(self):
         self.assertEqual(self.source.item_set.all().count(), 2)
+        # source items have their original documents
+        self.assertEqual(self.item.revision_set.all().count(), 1)
+        self.assertEqual(self.item2.revision_set.all().count(), 1)
 
         target = mommy.make('workspace.Workspace', name='Target Matter to get items', lawyer=self.lawyer)
         self.assertEqual(target.item_set.all().count(), 0)
@@ -33,13 +38,16 @@ class MatterCloneTest(TestCase):
 
         # test we now have the items
         self.assertEqual(target.item_set.all().count(), 2)
-        #s till has their original documents
-        self.assertTrue(all(i.revision_set.all().count() == 1 for i in target.item_set.all()))
+        # target items dont have any documents
+        import pdb;pdb.set_trace()
+        self.assertTrue(all(i.revision_set.all().count() == 0 for i in target.item_set.all()))
 
         # test that the slugs are all unique
         self.assertTrue(all(str(i.slug) not in self.source.item_set.all().values('slug') for i in target.item_set.all()))
+        self.assertTrue(all(str(i.pk) not in self.source.item_set.all().values('pk') for i in target.item_set.all()))
 
         # test that the source also still has them
         self.assertEqual(self.source.item_set.all().count(), 2)
-        #s till has their original documents
-        self.assertEqual(self.item.revision_set.all().count(), 2)
+        # source items still has their original documents
+        self.assertEqual(self.item.revision_set.all().count(), 1)
+        self.assertEqual(self.item2.revision_set.all().count(), 1)
