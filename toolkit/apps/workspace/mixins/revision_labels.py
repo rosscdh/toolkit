b1b606681e009799ccd2f981cd3913b832c25ef9
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-14 12:47:50 +0200

    Fixing edge-cases and various problems with matter.stadus_labels that i discovered.. the should not have been class methods as that makes them a single value for ALL users which is not quite right.

--- toolkit/apps/workspace/mixins/revision_labels.py
+++ toolkit/apps/workspace/mixins/revision_labels.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from toolkit.utils import _class_importer
 from collections import OrderedDict
 
 
@@ -6,9 +7,14 @@ class RevisionLabelMixin(object):
     """
     Mixin to allow changing the status names of revisions (and getting them)
     """
+    _revision_class = 'toolkit.core.attachment.models.Revision'
+
     @property
     def status_labels(self):
-        return self.data.get('status_labels', OrderedDict())
+        if type(self.__class__._revision_class) in [str, unicode]:
+            self.__class__._revision_class = _class_importer(self.__class__._revision_class)
+
+        return self.data.get('status_labels', self.__class__._revision_class.REVISION_STATUS.get_choices_dict())
 
     @status_labels.setter
     def status_labels(self, value):
@@ -20,3 +26,21 @@ class RevisionLabelMixin(object):
             choices[k] = value[k]
 
         self.data['status_labels'] = choices
+
+    @property
+    def default_status_index(self):
+        default_index = 0
+        #
+        # Get the actual default index; in case the user deletes all indexes
+        # except index 3 then 3 should be the default
+        #
+        for index, value in self.status_labels.iteritems():
+            if value not in [None, '']:
+                default_index = index
+                break
+
+        return default_index
+
+    @property
+    def default_status(self):
+        return self.status_labels[self.default_status_index]
