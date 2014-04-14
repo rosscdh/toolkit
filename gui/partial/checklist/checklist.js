8bbbc775b66dfded6d70f3a6ecd74b190cf097cb
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2014-04-14 13:32:24 +0200

    Use intercom_user_hash provided from api

--- gui/partial/checklist/checklist.js
+++ gui/partial/checklist/checklist.js
@@ -153,13 +153,12 @@ angular.module('toolkit-gui')
 
         $scope.initializeIntercom = function(currUser){
             $log.debug(currUser);
-            var user_hash = $window.CryptoJS.HmacSHA256(currUser.email, 'MZCesCDxkDrYdfX8HocAB2F6V5aZzCm-DuF7lyR5');
 
             Intercom.boot({
                 email: currUser.email,
                 created_at: new Date().getTime()/1000,
                 app_id: INTERCOM_APP_ID,
-                user_hash: user_hash.toString()
+                user_hash: currUser.intercom_user_hash
             });
 
             //Intercom.show();
