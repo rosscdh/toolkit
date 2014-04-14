07c3381c8165ed9621a7ac9ec730a0b44f7a0fe0
Jamie Connolly <jamie@jamieconnolly.com>
2014-04-14 16:06:39 +0100

    Change the Django side to use username as the user id

--- toolkit/apps/default/templatetags/intercom_tags.py
+++ toolkit/apps/default/templatetags/intercom_tags.py
@@ -7,19 +7,12 @@ register = template.Library()
 
 
 def _get_intercom_user_hash(user_identifier):
-    """
-    user_identifier should be user.pk, have implemented like this as at this time
-    we are using email which is wrong waiting on jamies api fix to set the user_id 
-    at intercom
-    @TODO check this with J and make sure weve set the user_id at intercom
-    then change user_identifier to be user.pk
-    """
     return hmac.new(settings.INTERCOM_APP_SECRET, user_identifier, digestmod=hashlib.sha256).hexdigest()
 
 
 @register.simple_tag
-def intercom_user_hash(email):
+def intercom_user_hash(username):
     """
     Generate the user_hash to turn on secure mode for Intercom.
     """
-    return _get_intercom_user_hash(user_identifier=email)
+    return _get_intercom_user_hash(user_identifier=username)
