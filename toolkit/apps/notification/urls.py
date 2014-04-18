a1710e8204a44fd1db00628c332be674fe0d2a1e
marbur <marius.burfey@ambient-innovation.com>
2014-04-18 14:44:47 +0200

    repaired notification-tests

--- toolkit/apps/notification/urls.py
+++ toolkit/apps/notification/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     url(r'^endpoints/inbox/all/read/$', login_required(MarkAllAsReadEndpoint.as_view()), name='inbox-mark-all-read'),
     url(r'^endpoints/inbox/(?P<pk>\d+)/read/$', login_required(InboxViewSet.as_view()), name='inbox-read'),
     #
-    url(r'^read/$', login_required(ReadNotificationsView.as_view()), name='default'),
+    url(r'^read/$', login_required(ReadNotificationsView.as_view()), name='read-notifications'),
     url(r'^$', login_required(InboxNotificationsView.as_view()), name='default'),
 )
