9f9c9a75f682d0bb78b6aaf38b47ad9caf8fc9a5
marbur <marius.burfey@ambient-innovation.com>
2014-04-22 11:19:27 +0200

    revision slug added to item-revision-link

--- toolkit/core/item/models.py
+++ toolkit/core/item/models.py
@@ -100,9 +100,14 @@ class Item(IsDeletedMixin,
 
     def get_full_user_review_url(self, user, version_slug):
         # returns url to item AND revision
-        # example: /matters/test-matter-1/#/checklist/e89403f273a045cd8a0ca7e7dd2bc383:/review/1d5d8b3ac969415e941aab3dd2ce41e8/BR45Ps8PPplMXg%3D%3D/
+        # outdated example: /matters/test-matter-1/#/checklist/e89403f273a045cd8a0ca7e7dd2bc383:/review/1d5d8b3ac969415e941aab3dd2ce41e8/BR45Ps8PPplMXg%3D%3D/
+        # example: /matters/test-matter-1/#/checklist/e89403f273a045cd8a0ca7e7dd2bc383/v3/review/1d5d8b3ac969415e941aab3dd2ce41e8/BR45Ps8PPplMXg%3D%3D/
+
+        # instead we could use the reviewer ID if we added it to the reviewer-serializer. or if we use the non-absolute
+        # user_review_url as identifier, we must add it to the review
+        
         review_document_link = self.get_user_review_url(user=user, version_slug=version_slug)
-        return "%s:%s" % (self.get_absolute_url(), review_document_link)
+        return "%s/%s%s" % (self.get_absolute_url(), version_slug, review_document_link)
 
     @property
     def client(self):
