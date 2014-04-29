e2e50dfebfcb9b4b719f60000fa46a20835e9741
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-04-29 10:40:22 +0200

    merge and rebuilt guy

---
+++ gui/dist/partial/request-review/request-review-spec.js
@@ -0,0 +1,18 @@
+describe('RequestreviewCtrl', function() {
+
+	beforeEach(module('toolkit-gui'));
+
+	var scope,ctrl;
+
+    beforeEach(inject(function($rootScope, $controller) {
+      scope = $rootScope.$new();
+      ctrl = $controller('RequestreviewCtrl', {$scope: scope});
+    }));	
+
+	it('should ...', inject(function() {
+
+		expect(1).toEqual(1);
+		
+	}));
+
+});
