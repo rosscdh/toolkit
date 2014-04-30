76bb6b2fe501e9864e833d35a25572ca1fb42437
leejsinclair <lee.j.sinclair@gmail.com>
2014-04-30 19:56:00 +1000

    Added JSDoc comments

--- gui/partial/checklist/checklist.js
+++ gui/partial/checklist/checklist.js
@@ -602,7 +602,17 @@ angular.module('toolkit-gui')
 			}
 		};
 
-		// showAddCategoryForm
+		/**
+		 * Sets an index value used to display/hide add category form
+		 *
+		 * @name 				showAddCategoryForm
+		 *
+		 * @param {Object} cat Catgory object
+		 *
+		 * @private
+		 * @method				showEditCategoryForm
+		 * @memberof			ChecklistCtrl
+		 */
 		$scope.showAddCategoryForm = function(index) {
 			if ($scope.data.showAddCategoryForm !== index) {
 				$scope.data.showAddCategoryForm = index;
