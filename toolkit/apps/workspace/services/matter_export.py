3263bafbd445cae2eab2873f87e3f0873d507244
marbur <marius.burfey@ambient-innovation.com>
2014-05-07 14:27:43 +0200

    comments & cleanup

--- toolkit/apps/workspace/services/matter_export.py
+++ toolkit/apps/workspace/services/matter_export.py
@@ -13,7 +13,7 @@ from toolkit.apps.matter.mailers import MatterExportFinishedEmail
 from toolkit.core.services.zip import ZipService
 
 
-MATTER_EXPORT_DAYS = getattr(settings, 'MATTER_EXPORT_DAYS', 3)
+MATTER_EXPORT_DAYS_VALID = getattr(settings, 'MATTER_EXPORT_DAYS_VALID', 3)
 
 
 class MatterExportService(object):
@@ -22,34 +22,36 @@ class MatterExportService(object):
         self.needed_revisions = []
         self.needed_files = []
 
+    def get_zip_filename(self, token_data):
+        # returns the filename the created .zip should have
+        # in in a function to get called from the download-view without instantiating an object
+        return '%s_%s_%s' % (token_data.get('matter_slug'), token_data.get('user_pk'), token_data.get('valid_until'))
+
     def ensure_needed_files_list(self):  # TODO: perhaps rename to list_executed_files (depending on which files we collect)
-        for item in self.matter.item_set.all():  # all items must be completed, otherwise the button wasn't shown
+        # collects all latest_revisions with the correct state
+        # TODO: check which revisions shall be exported
+        for item in self.matter.item_set.all():
             if item.latest_revision:
                 self.needed_revisions.append(item.latest_revision)
 
     def ensure_files_exist_locally(self):
+        # make sure the files exist locally
         for needed_revision in self.needed_revisions:
             # download latest_revision
             needed_revision.ensure_file()
             self.needed_files.append(needed_revision.get_document())
 
-    def ensure_list_of_existing_files(self):
-        self.ensure_needed_files_list()
-        self.ensure_files_exist_locally()
-
-    def get_zip_filename(self, token_data):
-        # in in a function to get called from the download-view without instantiating an object
-        return '%s_%s_%s' % (token_data.get('matter_slug'), token_data.get('user_pk'), token_data.get('valid_until'))
-
     def create_zip(self, filename):
-        # zip collection
+        # zip all needed files to filename
         zip_service = ZipService(filename)
         zip_service.add_file(self.needed_files)
         return zip_service.compress()
 
     def send_email(self, token):
-        # download_link = reverse('list')
+        # send the token-link to the owning lawyer
+
         # download_link = reverse('download-exported', args=(token, ))
+        # TODO: check why this is not working
 
         download_link = 'asdf'
 
@@ -60,16 +62,15 @@ class MatterExportService(object):
         m.process()
 
     def process(self):
-        # collect the needed revisions and put them in self.needed_files
-        self.ensure_list_of_existing_files()
+        # collect the needed revisions and put them in self.needed_files; make sure they exist on local disk
+        self.ensure_needed_files_list()
+        self.ensure_files_exist_locally()
 
-        # put everything needed to find the file into the token
-        valid_until = (datetime.datetime.now() + datetime.timedelta(days=MATTER_EXPORT_DAYS)).isoformat()
+        # put everything needed to find the file in AWS into the token
+        valid_until = (datetime.datetime.now() + datetime.timedelta(days=MATTER_EXPORT_DAYS_VALID)).isoformat()
         token_data = {'matter_slug': self.matter.slug,
                       'user_pk': self.matter.lawyer.pk,
                       'valid_until': valid_until}
-
-        token = signing.dumps(token_data, salt=settings.SECRET_KEY)
 
         # zip everything in self.needed_files
         zip_filename = self.get_zip_filename(token_data)
@@ -78,7 +79,8 @@ class MatterExportService(object):
         # upload to AWS as zip_filename
         s3boto_storage = S3BotoStorage()
         with default_storage.open(zip_file_path) as myfile:
-            result = s3boto_storage.save('exported_matters/%s.zip' % zip_filename, myfile)  # seems to need open file instead of path
+            result = s3boto_storage.save('exported_matters/%s.zip' % zip_filename, myfile)
 
         # send token-link via email
+        token = signing.dumps(token_data, salt=settings.SECRET_KEY)
         self.send_email(token)
