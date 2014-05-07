0668ef363f377171fad3ca38aac60e40e9f59bef
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-07 11:03:36 +0200

    moved hs methods into its own mixin

--- toolkit/apps/sign/models.py
+++ toolkit/apps/sign/models.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.core.files.storage import default_storage
 
 from rulez import registry as rulez_registry
 
-# NB! hellosign is not the same lib as hello_sign
-from hellosign import HelloSignUnclaimedDraftDocumentSignature
+
 # django wrapper hello_sign imports
-from hello_sign.models import HelloSignRequest
 from hello_sign.mixins import ModelContentTypeMixin, HelloSignModelMixin
-from hello_sign.services import HelloSignService
 
 from toolkit.core.mixins import IsDeletedMixin
 
@@ -20,13 +16,13 @@ from toolkit.apps.review.mixins import UserAuthMixin
 
 from .managers import SignDocumentManager
 from .mailers import SignerReminderEmail
+from .mixins import HelloSignOverridesMixin
 
 from storages.backends.s3boto import S3BotoStorage
 
 from uuidfield import UUIDField
 from jsonfield import JSONField
 
-import urlparse
 import logging
 import datetime
 logger = logging.getLogger('django.request')
@@ -35,6 +31,7 @@ logger = logging.getLogger('django.request')
 class SignDocument(IsDeletedMixin,
                    UserAuthMixin,
                    HelloSignModelMixin,
+                   HelloSignOverridesMixin,
                    ModelContentTypeMixin,
                    models.Model):
     """
@@ -53,20 +50,6 @@ class SignDocument(IsDeletedMixin,
     class Meta:
         # @BUSINESS RULE always return the newest to oldest
         ordering = ('-id',)
-
-    def __unicode__(self):
-        return u'%s' % str(self.slug)
-
-    def get_absolute_url(self, user):
-        auth_key = self.get_user_auth(user=user)
-        if auth_key is not None:
-            return reverse('sign:sign_document', kwargs={'slug': self.slug, 'auth_slug': auth_key})
-        return None
-
-    def complete(self, is_complete=True):
-        self.is_complete = is_complete
-        self.save(update_fields=['is_complete'])
-    complete.alters_data = True
 
     @property
     def signer_has_viewed(self):
@@ -124,16 +107,23 @@ class SignDocument(IsDeletedMixin,
             logger.error('no reviewer found for ReviewDocument: %s' % self)
             return None
 
-    def get_hs_service(self):
-        """
-        OVERRIDDEN to return the HelloSignUnclaimedDraftDocumentSignature as default
-        """
-        return HelloSignService(HelloSignSignatureClass=HelloSignUnclaimedDraftDocumentSignature,  # Passed in to override the default
-                                document=self.hs_document(),
-                                title=self.hs_document_title(),
-                                invitees=self.hs_signers(),
-                                subject=self.hs_subject(),
-                                message=self.hs_message())
+    @property
+    def signing_request(self):
+        return self.hellosign_requests().first()
+
+    def __unicode__(self):
+        return u'%s' % str(self.slug)
+
+    def get_absolute_url(self, user):
+        auth_key = self.get_user_auth(user=user)
+        if auth_key is not None:
+            return reverse('sign:sign_document', kwargs={'slug': self.slug, 'auth_slug': auth_key})
+        return None
+
+    def complete(self, is_complete=True):
+        self.is_complete = is_complete
+        self.save(update_fields=['is_complete'])
+    complete.alters_data = True
 
     def send_for_signing(self, **kwargs):
         kwargs.update({
@@ -141,68 +131,6 @@ class SignDocument(IsDeletedMixin,
         })
 
         return super(SignDocument, self).send_for_signing(**kwargs)
-
-    def hs_post_process_result(self, resp):
-        result = resp.json()
-        #
-        # Clean ugly HS namespace
-        #
-        result = result.get('unclaimed_draft', result)
-
-        if 'claim_url' in result:
-            # append client_id to the claim_url attrib
-            result['claim_url_with_client_id'] = '%s&client_id=%s' % (result['claim_url'], settings.HELLOSIGN_CLIENT_ID)
-
-        return result
-
-    def hs_record_result(self, result):
-        """
-        Because HS's api is so crap they dont give us an identifying GUID as part
-        of the standard json, instead we have to manually parse the gui out of their crappy
-        claim_url response, wasting precious cycles
-        """
-        claim_url = result.get('claim_url') # get claim url so we can **manually** (wtf hellosign) extract the gui
-        parsed_url = urlparse.urlparse(claim_url)
-        parsed_url_query = urlparse.parse_qs(parsed_url.query)
-        unclaimed_draft_guid = parsed_url_query.get('guid')
-
-        if unclaimed_draft_guid:
-            if type(unclaimed_draft_guid) in [list]:
-                unclaimed_draft_guid = unclaimed_draft_guid[0]  # its a list so get the first one as the guid
-
-            hs_request_object, is_new = HelloSignRequest.objects.get_or_create(unclaimed_draft_guid=unclaimed_draft_guid,
-                                                                               content_object_type=self.get_content_type_object(),
-                                                                               object_id=self.pk)
-            # save the result
-            hs_request_object.data = result
-            # save the field
-            hs_request_object.save(update_fields=['data'])
-
-            return hs_request_object
-        return None
-
-
-    def hs_subject(self):
-        return 'Signature Request for: %s' % self.document.name
-
-    def hs_document_title(self):
-        """
-        Method to set the document title, displayed in the HelloSign Interface
-        """
-        return self.document.name
-
-    def hs_document(self):
-        """
-        Return the document to be sent for signing
-        Ties in with HelloSignModelMixin method
-        """
-        return default_storage.open(self.document.executed_file)
-
-    def hs_signers(self):
-        """
-        Return a list of invitees to sign
-        """
-        return [{'name': u.get_full_name(), 'email': u.email} for u in self.signers.all()]
 
     def download_if_not_exists(self):
         """
