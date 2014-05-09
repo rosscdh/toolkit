93db5670d0689c5ee0a9a999b85bca73322901c9
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-09 14:14:42 +0200

    refactored matter_export into the matter app and out of workspace

---
+++ toolkit/apps/matter/services/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from .matter_export import MatterExportService
+from .matter_clone import MatterCloneService
+from .matter_removal import MatterRemovalService
+from .matter_participant_removal import MatterParticipantRemovalService
