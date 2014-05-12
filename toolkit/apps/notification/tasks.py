f2bd4b4e4490898cb931517df2cae90fc9e638e5
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-12 12:57:16 +0200

    better logging

--- toolkit/apps/notification/tasks.py
+++ toolkit/apps/notification/tasks.py
@@ -4,10 +4,14 @@ from toolkit.celery import app
 
 from .services import PusherPublisherService
 
+import logging
+logger = logging.getLogger('django.request')
+
 
 @app.task
 def youve_got_notifications(username, event, *args, **kwargs):
     if settings.PROJECT_ENVIRONMENT not in ['test']:
+        logger.info('Sending youve_got_notifications: %s' % username)
         pusher = PusherPublisherService(channel=username,
                                         event=event)
         pusher.process(**kwargs)
