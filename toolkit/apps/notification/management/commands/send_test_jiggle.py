21b129c922a0e94028630b47010c9e34219e7564
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-12 10:50:12 +0200

    pusher test cli command

---
+++ toolkit/apps/notification/management/commands/send_test_jiggle.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
+from django.core.management.base import BaseCommand, CommandError
+
+from toolkit.apps.notification.tasks import youve_got_notifications
+
+import pprint
+
+
+class Command(BaseCommand):
+    """
+    testing command to send the youve got notifications event for lees pusher integration
+    """
+    args = '<username>'
+    help = "enter the username for which to send the test jiggle"
+
+    def handle(self, *args, **options):
+        username = None
+
+        try:
+            username = args
+        except IndexError:
+            #
+            # Allow default to be used
+            #
+            PPP.pprint('Using default webhook_url sign:hellosign_webhook_event: %s' % self.callback_url)
+
+        if username:
+            youve_got_notifications(
+                     username=username,
+                     event='notification',
+                     detail='You have %d new notifications' % 10000000)
