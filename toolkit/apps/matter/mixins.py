ee20af6f31c1231b6bb945085a9b3478df5b36a2
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-13 18:48:10 +0200

    cleaned up implementation into mixin

--- toolkit/apps/matter/mixins.py
+++ toolkit/apps/matter/mixins.py
@@ -2,6 +2,8 @@
 from django.utils.timezone import utc
 from dateutil.parser import parse as dateutil_parse
 
+import datetime
+
 
 def _ensure_utc(date):
     return dateutil_parse(date).replace(tzinfo=utc)  # ensure is utc
@@ -11,6 +13,26 @@ class MatterExportMixin(object):
     """
     Mixin that provides Matter export info
     """
+    def export_matter(self, requested_by):
+        from toolkit.tasks import run_task
+        from toolkit.apps.matter.tasks import _export_matter
+
+        #
+        # Reset Pending Export
+        #
+        export_info = self.export_info
+        export_info.update({
+            'is_pending_export': True,
+            'last_export_requested': datetime.datetime.utcnow().isoformat(),
+            'last_export_requested_by': requested_by.get_full_name(),
+        })
+        self.save(update_fields=['data'])
+
+        # start the process
+        run_task(_export_matter, matter=self, requested_by=requested_by)
+        # record the event
+        self.actions.started_matter_export(user=requested_by)
+
     @property
     def export_info(self):
         data = self.data.get('export', {
--- toolkit/apps/matter/tasks.py
+++ toolkit/apps/matter/tasks.py
@@ -7,16 +7,5 @@ import datetime
 
 @app.task
 def _export_matter(matter, requested_by):
-    #
-    # Reset Pending Export
-    #
-    export_info = matter.export_info
-    export_info.update({
-        'is_pending_export': True,
-        'last_export_requested': datetime.datetime.utcnow().isoformat(),
-        'last_export_requested_by': requested_by.get_full_name(),
-    })
-    matter.save(update_fields=['data'])
-
     matter_export_service = MatterExportService(matter=matter, requested_by=requested_by)
     matter_export_service.process()
