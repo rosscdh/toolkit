7345e34e4fe49ce84c4b6cdb5e9ec12f38c80ff9
marbur <marius.burfey@ambient-innovation.com>
2014-05-21 09:16:22 +0200

    added message-test

--- toolkit/core/item/tests/reminder.py
+++ toolkit/core/item/tests/reminder.py
@@ -8,6 +8,7 @@ from toolkit import settings
 from toolkit.casper.workflow_case import BaseScenarios
 
 from ...services.reminder import ReminderService
+from toolkit.core.services.lawpal_abridge import LawPalAbridgeService
 
 
 class ReminderServiceTest(BaseScenarios, TestCase):
@@ -36,3 +37,17 @@ class ReminderServiceTest(BaseScenarios, TestCase):
         items_to_remind = reminder_service.collect_items()
 
         self.assertItemsEqual(items_to_remind, [])
+
+    def test_reminder_template(self):
+        # create item
+        item = mommy.make('item.Item',
+                          name='Test Item #1',
+                          matter=self.matter,
+                          date_due=datetime.datetime(year=2000, month=1, day=1))
+        message_data = {
+            'item': item
+        }
+        message = LawPalAbridgeService.render_reminder_template(**message_data)
+
+        self.assertEqual(message,
+                         u'<p>Action required</p>\n<p style="color: red">Test Item #1 is not closed yet although it\'s due date is near: 2000-01-01</p>')
