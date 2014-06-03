ee4e5c74b99ac769d64599295d0ddca445b2bfff
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-06-03 14:39:36 +0200

    moved base.html js into core.js

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -322,6 +322,24 @@ PIPELINE_CSS = {
   }
 }
 PIPELINE_JS = {
+    'core': {
+        'source_filenames': (
+            'js/jquery.ui.touch-punch.min.js',
+            'js/bootstrap.min.js',
+            'js/bootstrap-select.js',
+            'js/bootstrap-switch.js',
+            'js/flatui-checkbox.js',
+            'js/flatui-radio.js',
+            'js/jquery.tagsinput.js',
+            'js/jquery.placeholder.js',
+            'js/bootstrap-typeahead.js',
+            'js/parsley-1.2.4.min.js',
+            'js/parsley-form.js',
+            'js/application.js',
+        ),
+        'output_filename': 'js/core.js',
+    },
+
     'reactjs': {
         'source_filenames': (
             'js/react-0.10.0.js',
