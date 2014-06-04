659d847349d85f673ed4e1b0be89606e2bf9062f
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-06-04 09:01:07 +0200

    moved hs urls back as sub of sign app

--- toolkit/apps/sign/urls.py
+++ toolkit/apps/sign/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     # Base
     url(r'^claim/(?P<slug>[\w-]+)/$', ClaimSignRevisionView.as_view(), name='claim_sign_document'),
     url(r'^signature/(?P<slug>[\w-]+)/(?P<username>.*)/$', SignRevisionView.as_view(), name='sign_document'),
+    # HelloSign
+    url(r'^hellosign/', include('hello_sign.urls')),
 )
