0b6bfce94d566685426c752b4a3976144270bde0
Yekaterina Novikova <ykatesque@gmail.com>
2014-06-19 12:33:51 -0700

    date alignment

--- gui/directive/sticky-elements/sticky-elements.js
+++ gui/directive/sticky-elements/sticky-elements.js
@@ -14,52 +14,41 @@ angular.module('toolkit-gui').directive("sticky", [ '$window', function($window)
 	return {
 		'scope': {
 			'scrollelement': '=',
-			'offset': '=',
-			'debug': '='
+			'tolerance': '='
 		},
 		'restrict': "A",
 		'link': function(scope, element/*, attrs*/) {
 			var scrollerNode = document.getElementById(scope.scrollelement)||$window;
 			var parent = element.parent();
-			var offset = scope.offset||55;
+			var tolerance = scope.tolerance||57;
 
-			function processPositions() {
-				var borderHeight = parseInt(element.css('border-top-width')) + parseInt(element.css('border-bottom-width'));
-				var marginHeight = parseInt(element.css('margin-bottom')) + parseInt(element.css('margin-top'));
-				var elementHeight = element.height() + parseInt(element.css('padding-top')) + parseInt(element.css('padding-bottom')) + borderHeight;
+			//console.log(element.css('padding-top'));
+
+			angular.element(scrollerNode).bind("scroll", function() {
+				var elementHeight = element.height() + parseInt(element.css('padding-top')) + parseInt(element.css('padding-bottom'));
 				var parentHeight = parent.height() + parseInt(parent.css('padding-top')) + parseInt(parent.css('padding-bottom'));
-				var currentParentOffset = parent.offset().top;
+				var currentParentOffset = parent.offset().top - elementHeight;
 				var elementOffset = element.offset().top;
 				var zero = elementHeight;
 
-				if(scope.debug) {
-					console.log(currentParentOffset);
-				}
+				//console.log(elementHeight, parentHeight);
 
 				// If parent within the height of the top then stick element to top
 				if( currentParentOffset > (-1 * parentHeight) && (currentParentOffset) < zero ) {
 					element.addClass('sticky-fixed');
-					parent.css('padding-top', elementHeight + 'px');
 
-					if( parentHeight + currentParentOffset - elementHeight < offset ) {
-						// Scroll item up nicely
-						element.css('top', (parentHeight + currentParentOffset - elementHeight - borderHeight) + 'px' );
-					} else {
-						// Set item position to top
-						element.css('top', offset + 'px' );
-					}
+					parent.css('padding-top', elementHeight + 'px');
 				} else {
 					element.removeClass('sticky-fixed');
 					parent.css('padding-top', 0);
-					element.css('top', 0 );
 				}
-			}
-
-			angular.element(scrollerNode).bind("scroll", function() {
-				processPositions();
 			});
 
-			processPositions();
+			// Remove event bindings on scroll
+			scope.$on('$destroy', function() {
+				angular.element(scrollerNode).unbind("scroll");
+			});
+
 		}
 	};
 }]);
