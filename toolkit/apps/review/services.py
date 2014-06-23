36d1088b12f8fc9bd3408103c1f1fbfc9bb03330
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-06-23 11:09:40 +0200

    added ensure file to crocodoc process to allow us to get the uuid

--- toolkit/apps/review/services.py
+++ toolkit/apps/review/services.py
@@ -22,10 +22,13 @@ class CrocodocLoaderService(object):
         self.user = user
         self.reviewdocument = reviewdocument
         self.ensure_reviewer()
+
+        self.reviewdocument.ensure_file() ## ensure we have a local copy of the file
+
         self.service = CrocoDocConnectService(document_object=self.reviewdocument.document,
                                               app_label='attachment',
                                               field_name='executed_file',
-                                              upload_immediately=self.crocodoc_uuid_recorded,  # this is handled by the ensure_local_file method
+                                              upload_immediately=self.crocodoc_uuid_recorded is False,  # this is handled by the ensure_local_file method
                                               # important for sandboxing the view to the specified reviewer
                                               reviewer=self.reviewdocument.reviewer)
 
