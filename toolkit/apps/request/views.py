9e06cac0a28a31160c37208a54b9ac21cdee1c5d
Jamie Connolly <jamie@jamieconnolly.com>
2014-07-08 17:47:18 +0100

    Add in the base views for completed requests

--- toolkit/apps/request/views.py
+++ toolkit/apps/request/views.py
@@ -3,8 +3,25 @@ from django.views.generic import ListView
 from toolkit.core.item.models import Item
 
 
-class RequestListView(ListView):
+class CompletedRequestListView(ListView):
     template_name = 'request/request_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CompletedRequestListView, self).get_context_data(**kwargs)
+        context.update({ 'show_completed': True })
+        return context
+
+    def get_queryset(self):
+        return Item.objects.my_requests(self.request.user, completed=True)
+
+
+class OpenRequestListView(ListView):
+    template_name = 'request/request_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(OpenRequestListView, self).get_context_data(**kwargs)
+        context.update({ 'show_completed': False })
+        return context
 
     def get_queryset(self):
         return Item.objects.my_requests(self.request.user)
