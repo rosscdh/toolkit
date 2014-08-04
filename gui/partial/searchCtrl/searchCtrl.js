3156aac4d509883322f9c90a1be3efd2310808df
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-08-04 14:55:42 +0200

    added timeout delay for search to handle multiple key entries and not overloading the system

--- gui/partial/searchCtrl/searchCtrl.js
+++ gui/partial/searchCtrl/searchCtrl.js
@@ -12,6 +12,7 @@ angular.module('toolkit-gui').controller('SearchCtrl',[
 		var routeParams = smartRoutes.params();
 
 		$scope.data = {
+			'searchPromise': null,
 			'searchResults': [],
 			'matterSlug': routeParams.matterSlug,
 			'term': '',
@@ -22,17 +23,27 @@ angular.module('toolkit-gui').controller('SearchCtrl',[
 
 			if( keyCode !== 13 ) {
 				// Search
-				searchService.filter( $scope.data.matterSlug, $scope.data.term ).then(
-					function success( results ) {
-                        $log.debug("Found results: " + results.length);
-						$scope.data.searchResults = results;
-						$scope.data.display = true;
-					},
-					function error( data ) {
-						$scope.data.searchResults = [];
-						$scope.data.display = false;
-					}
-				);
+
+				// kill the promise if another keypress comes in
+				if ( $scope.searchPromise !== null ) {
+					$timeout.cancel($scope.searchPromise);
+				}
+				// delay the search pending another term entry on keyup
+				$scope.searchPromise = $timeout(function () {
+
+					searchService.filter( $scope.data.matterSlug, $scope.data.term ).then(
+						function success( results ) {
+	                        $log.debug("Found results: " + results.length);
+							$scope.data.searchResults = results;
+							$scope.data.display = true;
+						},
+						function error( data ) {
+							$scope.data.searchResults = [];
+							$scope.data.display = false;
+						}
+					);
+
+				},300);
 
 			} else if( $scope.data.searchResults.length > 0 ) {
 				// Select item
